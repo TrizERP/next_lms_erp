07f067d5543b4a54f2148681d98f055d57178d28
ronakgolakiya <ronakgolakiya12@gmail.com>
2023-04-15 00:35:01 +0530

    fix: excel change

--- public/excel_upload/export_xlsx.php
+++ public/excel_upload/export_xlsx.php
@@ -175,11 +175,16 @@ if (isset($_REQUEST['sub_institute_iderp']) && $_REQUEST['sub_institute_iderp'] 
                                 //print_r($titleArr);
                                 $fileName = exportExcel($titleArr, $stuSqlRet, $exportfileName, $excelVersion, $sheetPaasWord);
 
-                                header('Location: export_xlsx.php?fileName=' . $fileName);
-//
+//                                header('Location: export_xlsx.php?fileName=' . $fileName);
+                                ?>
+                                <script>
+                                    console.log(325);
+                                    window.location.href = "export_xlsx.php?fileName=' <?php echo $fileName; ?> '";
+                                </script>
+                                <?php
 //                                $valid_str = "";
-//                                $valid_str .= "<script language='javascript'>";
-//                                $valid_str .= 'console.log(324)';
+//                                $valid_str .= "<script>";
+//                                $valid_str .= "console.log(324)";
 //                                $valid_str .= 'window.location.href = "export_xlsx.php?fileName=' . $fileName . '"';
 //                                $valid_str .= "</script>";
 //                                echo $valid_str;
