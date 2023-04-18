57c6fed76ed430d0799fb845afaf78391d7985b9
ronakgolakiya <ronakgolakiya12@gmail.com>
2023-04-19 00:10:37 +0530

    fix: excel change

--- public/library/sysconfig.inc.php
+++ public/library/sysconfig.inc.php
@@ -311,27 +311,20 @@ define('REPO_BASE_DIR', SENAYAN_BASE_DIR.repository.DIRECTORY_SEPARATOR);
 // echo '<pre> DB_NAME '; print_r(DB_NAME);
 // echo '<pre> DB_PORT '; print_r(DB_PORT);
 // die;
-echo 'asas';
 
-if (extension_loaded('mysqli')) {
-    echo 'as23r23as';
-    die;
-    /* MYSQLI */
-    $dbs = @new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
-    if (mysqli_connect_error()) {
-        die('<div style="border: 1px dotted #FF0000; color: #FF0000; padding: 5px;">Error Connecting to Database. Please check your configuration</div>');
-    }
-} else {
-    echo '32d';
-    die;
-    /* MYSQL */
-    // require the simbio mysql class
-
-    include SIMBIO_BASE_DIR . 'simbio_DB/mysql/simbio_mysql.inc.php';
-    $dbs = @new simbio_mysql(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+//if (extension_loaded('mysqli')) {
+/* MYSQLI */
+$dbs = @new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+if (mysqli_connect_error()) {
+    die('<div style="border: 1px dotted #FF0000; color: #FF0000; padding: 5px;">Error Connecting to Database. Please check your configuration</div>');
 }
-echo 'asas';
-die;
+//} else {
+//    /* MYSQL */
+//    // require the simbio mysql class
+//
+//    include SIMBIO_BASE_DIR . 'simbio_DB/mysql/simbio_mysql.inc.php';
+//    $dbs = @new simbio_mysql(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+//}
 /* session login timeout in second */
 $sysconf['session_timeout'] = 7200;
 
