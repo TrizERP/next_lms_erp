4225b220198ccc1bd8101c836e0981a516879d56
Rajesh <rajesh.rafaliya@triz.co.in>
2023-04-29 15:38:23 +0530

    29-04-2023

--- app/Models/fees/fees_cancel/feesCancelModel.php
+++ app/Models/fees/fees_cancel/feesCancelModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class feesCancelModel extends Model
 {
+   public $timestamps = false;
     protected $table = 'fees_cancel';
 
     protected $fillable = [
