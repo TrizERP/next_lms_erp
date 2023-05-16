7c9207663daff86bf053425b000dc5bc662477ad
Krupali Desai <krupalidesai254@gmail.com>
2023-05-16 20:08:12 +0530

    Asset update

--- resources/views/includes/footerJs.blade.php
+++ resources/views/includes/footerJs.blade.php
@@ -184,24 +184,11 @@
 <script src="{{ asset("/admin_dep/js/custom.js") }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
-
-<!-- <script src="{{ asset("/plugins/bower_components/jquery/dist/jquery.min.js") }}"></script> -->
-<!-- Bootstrap Core JavaScript -->
-<!-- <script src="{{ asset("/admin_dep/bootstrap/dist/js/bootstrap.min.js") }}"></script> -->
-<!-- Menu Plugin JavaScript -->
-<!-- <script src="{{ asset("/plugins/bower_components/sidebar-nav/dist/sidebar-nav.min.js") }}"></script> -->
-<!--slimscroll JavaScript -->
-<!-- <script src="{{ asset("/admin_dep/js/jquery.slimscroll.js") }}"></script> -->
-<!--Wave Effects -->
-<!-- <script src="{{ asset("/admin_dep/js/waves.js") }}"></script> -->
 <!-- chartist chart -->
 <script src="{{ asset("/plugins/bower_components/chartist-js/dist/chartist.min.js") }}"></script>
-<script src="{{ asset("/plugins/bower_components/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js") }}"></script>
+{{-- <script src="{{ asset("/plugins/bower_components/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js") }}"></script> --}}
 <!-- Sparkline chart JavaScript -->
 <script src="{{ asset("/plugins/bower_components/jquery-sparkline/jquery.sparkline.min.js") }}"></script>
-<!-- Custom Theme JavaScript -->
-<!-- <script src="{{ asset("/admin_dep/js/custom.min.js") }}"></script> -->
-<!-- <script src="{{ asset("/admin_dep/js/dashboard1.js") }}"></script> -->
 
 <!--Style Switcher -->
 <!-- <script src="{{ asset("/plugins/bower_components/styleswitcher/jQuery.style.switcher.js") }}"></script> -->
