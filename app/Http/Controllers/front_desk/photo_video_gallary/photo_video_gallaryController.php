11a3c8ac72bce940e70df1216162180d5c003532
Rajesh <rajeshrafaliya@gmail.com>
2023-06-12 16:58:22 +0530

    photo gallary in mobile app

--- app/Http/Controllers/front_desk/photo_video_gallary/photo_video_gallaryController.php
+++ app/Http/Controllers/front_desk/photo_video_gallary/photo_video_gallaryController.php
@@ -564,28 +564,19 @@ class photo_video_gallaryController extends Controller
                 ->orderBy('p.date_', 'DESC')
                 ->get()->toArray();
 
-            if(!empty($data))
-            {
-                $new_data = [];
-                foreach ($data as $key => $val) {
-                    $new_data[$val->album_title]['album_title'] = $val->album_title;
-                    $new_data[$val->album_title]['album'][] = [
-                        "album_title" => $val->album_title,
-                        "title" => $val->title,
-                        "file_name" => $val->file_name,
-                        "date_" => $val->date_,
-                        "ai" => $val->ai,
-                        "type" => $val->type
-                    ];
+                $new_data = array();
+                foreach($data as $key => $val)
+                {
+                    $new_data[$val->album_title][] = $val;
+                }      
+                if(!empty($new_data)){
+                    $res['status'] = 1;
+                    $res['message'] = "Success";
+                    $res['data'] = $new_data;
+                }else{
+                    $res['status'] = 0;
+                    $res['message'] = "No Data Found";
                 }
-
-                $res['status'] = 1;
-                $res['message'] = "Success";
-                $res['data'] = array_values($new_data);
-            }else{
-                $res['status'] = 0;
-                $res['message'] = "No Data Found";
-            }
         } else {
             $res['status'] = 0;
             $res['message'] = "Parameter Missing";
