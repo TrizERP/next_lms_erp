6efe4cdae0761598582f4f130a10d9043d5b3cd2
minhazkhan60 <minhazkhan60@gmail.com>
2023-06-27 17:43:30 +0530

    in dynamic report vehicle report

--- app/Http/Controllers/report/dynamic_report/dynamic_report_controller.php
+++ app/Http/Controllers/report/dynamic_report/dynamic_report_controller.php
@@ -475,7 +475,24 @@ class dynamic_report_controller extends Controller
                             }
                         }
                     }
-                else {
+                    else{
+                        if ($main_module_name == "Transport") {
+                            $this->query = DB::table('transport_vehicle as tv');
+                            $main_table_initial = "tv";
+                            foreach ($sub_module_name as $id => $arr) {
+                                if ($arr->sub_module == "Vehicle") {
+                                    $tbltransport_join = [
+                                        'tv.driver'       => 'tdd.id',
+                                        'tv.sub_institute_id' => 'tdd.sub_institute_id',
+                                    ];
+                                    
+                                    $this->query->join('transport_driver_detail as tdd', function($join){
+                                        $join->whereRaw('tv.driver = tdd.id or tv.conductor = tdd.id and tv.sub_institute_id');
+                                    });
+                                }
+                            }
+                        }
+                    else {
                         if ($main_module_name == "Circular") {
                             $this->query = DB::table('circular as c');
                             $main_table_initial = "c";
@@ -495,6 +512,7 @@ class dynamic_report_controller extends Controller
                     }
                 }
             }
+        }
                 }
             }
         }
@@ -505,9 +523,12 @@ class dynamic_report_controller extends Controller
 // Standard,Chapter Name,Subject Name,Total Contents,Content Name,Total Sub Content,Sub Content Name
         $col = [];
         foreach ($all_detail["selected_fields"] as $id => $val) {
-
-            if ($all_fields_name[$val] == " Full Name") {
+            
+            if ($main_module_name !="Transport" && $all_fields_name[$val] == " Full Name") {
                 $col[] = DB::raw("concat_ws(' ',s.first_name,s.middle_name,s.last_name) as full_name");
+            }elseif($main_module_name =="Transport" && $all_fields_name[$val] == " Full Name")
+            {
+                $col[] = DB::raw("concat_ws(' ',tdd.first_name,tdd.last_name) as full_name");
             }
             elseif($all_fields_name[$val]=="Chapter Name"){
                 $col[] = DB::raw("GROUP_CONCAT(DISTINCT chm.chapter_name) as chapter_name");
