4885cfac08801a53f36842b29a6dad2c6be4b616
rp2164394 <rp2164394@gmail.com>
2023-06-29 15:19:00 +0530

    fees collect one tag for month

---
+++ database/migrations/2023_06_29_142644_add_show_month_to_fees_config_master.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fees_config_master', function (Blueprint $table) {
+            //
+            $table->integer('show_month')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('fees_config_master', function (Blueprint $table) {
+            //
+            $table->dropColumn('show_month');
+        });
+    }
+};
