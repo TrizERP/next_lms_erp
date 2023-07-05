054d926dd457a0f08c4b272db29ad634b80209bb
rp2164394 <rp2164394@gmail.com>
2023-07-05 16:59:13 +0530

    additional fees map month and head wise

--- app/Http/Controllers/fees/other_fee_map/other_fee_map_controller.php
+++ app/Http/Controllers/fees/other_fee_map/other_fee_map_controller.php
@@ -27,8 +27,15 @@ class other_fee_map_controller extends Controller
             }
         }
         $fee_month = FeeMonthId();
-        // echo ('<pre>');print_r($fee_month);exit;
+        $fees_title = fees_title::select('id', 'display_name', 'fees_title', 'mandatory', 'syear', 'other_fee_id')
+            ->where([
+                'sub_institute_id' => session()->get('sub_institute_id'),
+                'syear' => session()->get('syear'),
+                'fees_title_id' => 1
+            ])->orderBy('sort_order')->get();
         $data['data']['ddMonth'] = $fee_month;
+        $data['data']['heads'] = $fees_title;
+        
         $type = $request->input('type');
         return is_mobile($type, "fees/other_fee_map/show", $data, "view");
     }
@@ -40,52 +47,87 @@ class other_fee_map_controller extends Controller
      */
     public function create(Request $request)
     {
-        session(['month_id' => $_REQUEST['month_id']]);
+        // session(['month_id' => $_REQUEST['month_id']]);
         $type = $request->input('type');
+        $fees_heads = $request->fees_heads;
+        
      // controller.php
-$student_data = \App\Helpers\SearchStudent($_REQUEST['grade'],$_REQUEST['standard'],$_REQUEST['division'],'', '','', $_REQUEST['stu_name'],$_REQUEST['uniqueid'],$_REQUEST['mobile'],$_REQUEST['grno']);
-        // return $student_data;exit;
+        $student_data = \App\Helpers\SearchStudent($_REQUEST['grade'],$_REQUEST['standard'],$_REQUEST['division'],'', '','', $_REQUEST['stu_name'],$_REQUEST['uniqueid'],$_REQUEST['mobile'],$_REQUEST['grno']);
         $sub_institute_id = session()->get('sub_institute_id');
         $syear = session()->get('syear');
-        $mp_id = session()->get('month_id');
+        $mp_id = $request->month_id;
 
         $fees_breckoff = DB::table('fees_breakoff_other')
             ->where('sub_institute_id', $sub_institute_id)
             ->where('syear', $syear)
-            ->where('month_id', $mp_id)->get()->toArray();
-
-        $fees_title = fees_title::select('id', 'display_name', 'fees_title', 'mandatory', 'syear', 'other_fee_id')
+            ->whereRaw('month_id IN ("'.implode('","',$mp_id).'")')->get()->toArray();
+        // echo "<pre>";print_r($fees_breckoff);exit;
+        $fees_title['data'] = fees_title::select('id', 'display_name', 'fees_title', 'mandatory', 'syear', 'other_fee_id')
             ->where([
                 'sub_institute_id' => session()->get('sub_institute_id'),
                 'syear' => session()->get('syear'),
                 'fees_title_id' => 1
-            ])->orderBy('sort_order')->get()->toArray();
-
+            ])
+            ->whereRaw('id IN ("'.implode('","',$fees_heads).'")')
+            ->orderBy('sort_order')->get()->toArray();
+        $responce_arr['months_id'] = $request->month_id;
+              
         $responce_arr['grade'] = $_REQUEST['grade'];
         $responce_arr['standard'] = $_REQUEST['standard'];
         $responce_arr['division'] = $_REQUEST['division'];
-        foreach ($student_data as $id => $arr) {
+  
+        $months = [
+            1 => 'Jan', 2 => 'Feb', 3 => 'Mar', 4 => 'Apr', 5 => 'May', 6 => 'Jun', 7 => 'Jul', 8 => 'Aug', 9 => 'Sep',
+            10 => 'Oct', 11 => 'Nov', 12 => 'Dec',
+        ];
+        $fees_breckoff_grp = DB::table('fees_breakoff_other')
+        ->where('sub_institute_id', $sub_institute_id)
+        ->where('syear', $syear)
+        ->groupBy('month_id')->get()->toArray();
+        $month_names = [];
+        foreach ($mp_id as $id => $arr) {
+            $y = $arr / 10000;
+            $month = (int)$y;
+            $year = substr($arr, -4);
+            $month_names[$arr] = $months[$month] . "-" . $year;
+        }
 
+        foreach ($fees_title['data'] as $fees_title_item) {
+            $fee_type_id = $fees_title_item['fees_title'];
+
+            foreach ($mp_id as $arr) {
+                $month_id = $arr;
+                $month_name = $month_names[$month_id];
+                $fees_title['month'][] = $month_name . '/' . $fees_title_item['display_name'];
+                $fees_title['month_id'][] = $month_id;                
+            }
+        }
+
+        $responce_arr['month_head'] = $fees_title['month'];
+        // return $fees_title;exit;
+        foreach ($student_data as $id => $arr) {
             $responce_arr['stu_data'][$id]['sr.no'] = $id + 1;
             $responce_arr['stu_data'][$id]['name'] = $arr['first_name'] . ' ' . $arr['middle_name'] . ' ' . $arr['last_name'];
             $responce_arr['stu_data'][$id]['student_id'] = $arr['student_id'];
             $responce_arr['stu_data'][$id]['mobile'] = $arr['mobile'];
             $responce_arr['stu_data'][$id]['std'] = $arr['standard_name'];
             $responce_arr['stu_data'][$id]['div'] = $arr['division_name'];
-            foreach ($fees_title as $temp_id => $vals) {
+            foreach ($mp_id as $key=>$month_id){   
+            foreach ($fees_title['data'] as $temp_id => $vals) {
                 $amount = 0;
+                $month_name = $fees_title['month'][$temp_id];
                 foreach ($fees_breckoff as $bk_temp_id => $bk_vals) {
-                    if ($arr['student_id'] == $bk_vals->student_id &&
-                        $bk_vals->fee_type_id == $vals['other_fee_id']
-                    ) {
+                    if ($arr['student_id'] == $bk_vals->student_id && $bk_vals->fee_type_id == $vals['other_fee_id'] && $month_id == $bk_vals->month_id) {
                         $amount = $bk_vals->amount;
                     }
                 }
-                $responce_arr['stu_data'][$id][$vals['other_fee_id']] = $amount;
+                
+                $responce_arr['stu_data'][$id][$month_id][$vals['display_name']]['amount'] = $amount;
             }
         }
+        }
         $responce_arr['fees_title'] = $fees_title;
-// echo "<pre>";$responce_arr['fees_title'];exit;
+        
         return is_mobile($type, "fees/other_fee_map/add", $responce_arr, "view");
     }
 
@@ -101,9 +143,10 @@ $student_data = \App\Helpers\SearchStudent($_REQUEST['grade'],$_REQUEST['standar
             foreach ($_REQUEST['student_id'] as $student_id => $val) {
                 foreach ($_REQUEST['values'] as $student_id1 => $arr) {
                     if ($student_id == $student_id1) {
-
-                        foreach ($arr as $fee_type_id => $value) {
-                            DB::table('fees_breakoff_other')
+                        foreach ($arr as $month_id => $value) {
+                        foreach ($value as $fee_type_id => $amount) {
+                            // if exists then delete
+                           DB::table('fees_breakoff_other')
                                 ->where([
                                     'syear' => session()->get('syear'),
                                     'student_id' => $student_id,
@@ -111,9 +154,10 @@ $student_data = \App\Helpers\SearchStudent($_REQUEST['grade'],$_REQUEST['standar
                                     // 'grade_id' => $_REQUEST['grade'],
                                     // 'standard_id' => $_REQUEST['standard'],
                                     // 'section_id' => $_REQUEST['division'],
-                                    'month_id' => session()->get('month_id'),
+                                    'month_id' => $month_id,
                                     'sub_institute_id' => session()->get('sub_institute_id')
                                 ])->delete();
+                                //insert
                             DB::table('fees_breakoff_other')->insert(
                                 array(
                                     'syear' => session()->get('syear'),
@@ -122,11 +166,13 @@ $student_data = \App\Helpers\SearchStudent($_REQUEST['grade'],$_REQUEST['standar
                                     // 'grade_id' => $_REQUEST['grade'],
                                     // 'standard_id' => $_REQUEST['standard'],
                                     // 'section_id' => $_REQUEST['division'],
-                                    'month_id' => session()->get('month_id'),
-                                    'amount' => $value,
+                                    'month_id' => $month_id,
+                                    'amount' => $amount,
                                     'sub_institute_id' => session()->get('sub_institute_id')
                                 )
                             );
+                        
+                        }
                         }
                     }
                 }
@@ -143,52 +189,18 @@ $student_data = \App\Helpers\SearchStudent($_REQUEST['grade'],$_REQUEST['standar
         }
 
         $type = $request->input('type');
-        return is_mobile($type, "other_fee_map.index", $res, "redirect");
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return void
-     */
-    public function show($id)
-    {
-//
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return void
-     */
-    public function edit($id)
-    {
-//
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param int $id
-     * @return void
-     */
-    public function update(Request $request, $id)
-    {
-//
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return void
-     */
-    public function destroy($id)
-    {
-//
+        $fee_month = FeeMonthId();
+        $fees_title = fees_title::select('id', 'display_name', 'fees_title', 'mandatory', 'syear', 'other_fee_id')
+            ->where([
+                'sub_institute_id' => session()->get('sub_institute_id'),
+                'syear' => session()->get('syear'),
+                'fees_title_id' => 1
+            ])->orderBy('sort_order')->get();
+        $res['data']['ddMonth'] = $fee_month;
+        $res['data']['heads'] = $fees_title;
+        
+        $type = $request->input('type');
+        // return is_mobile($type, "fees/other_fee_map/show", $res, "view");
     }
 
 }
