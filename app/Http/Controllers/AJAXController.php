fcd3b48ea127059aaedaa371262786d7410ed901
rp2164394 <rp2164394@gmail.com>
2023-07-14 17:45:17 +0530

    fees collect if amount 0 then don't show head

--- app/Http/Controllers/AJAXController.php
+++ app/Http/Controllers/AJAXController.php
@@ -565,14 +565,16 @@ class AJAXController extends Controller
 
         foreach ($till_now_breckoff as $month_id => $fees_detail) {
             foreach ($fees_detail as $head_name => $arr) {
+                if($arr['amount'] !==0){
                 if (! isset($reg_bk_month_wise[$arr['title']])) {
                     $reg_bk_month_wise[$arr['title']] = 0;
                 }
                 $reg_bk_month_wise[$arr['title']] += $arr['amount'];
                 $final_bk_name[$arr['title']] = $head_name;
             }
+            }
         }
-
+        // return $final_bk_name;exit;
         foreach ($till_now_breckoff2 as $month_id => $fees_detail) {
             foreach ($fees_detail as $head_name => $arr) {
                 if (! isset($reg_bk_month_wise2[$arr['title']])) {
