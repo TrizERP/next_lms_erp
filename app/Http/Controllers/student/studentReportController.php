2d2d4b54faf2c1af1ddb0c9576fc0d451b2085f5
rp2164394 <rp2164394@gmail.com>
2023-07-19 15:37:36 +0530

    Optional subject in student report

--- app/Http/Controllers/student/studentReportController.php
+++ app/Http/Controllers/student/studentReportController.php
@@ -19,6 +19,29 @@ class studentReportController extends Controller
     public function index(Request $request)
     {
         $type = $request->input('type');
+        // $tblcustom_fields = DB::table("tblcustom_fields")
+        // ->where(["sub_institute_id" => session()->get('sub_institute_id'),"table_name" => "tblstudent"])
+        // ->pluck("field_label", "field_name");
+
+        // $tblcustom_fields['enrollment_no'] = 'Enrollment No';
+        // $tblcustom_fields['first_name'] = 'First Name';
+        // $tblcustom_fields['middle_name'] = 'Middle Name';
+        // $tblcustom_fields['last_name'] = 'Last Name';
+        // $tblcustom_fields['father_name'] = 'Father Name';
+        // $tblcustom_fields['mother_name'] = 'Mother Name';
+        // $tblcustom_fields['gender'] = 'Gender';
+        // $tblcustom_fields['dob'] = 'Birthdate';
+        // $tblcustom_fields['mobile'] = 'Mobile';
+        // $tblcustom_fields['mother_mobile'] = 'Mother Mobile';
+        // $tblcustom_fields['email'] = 'Email';
+        // $tblcustom_fields['username'] = 'Username';
+        // $tblcustom_fields['admission_year'] = 'Admission Year';
+        // $tblcustom_fields['admission_date'] = 'Admission Date';
+        // $tblcustom_fields['city'] = 'City';
+        // $tblcustom_fields['state'] = 'State';
+        // $tblcustom_fields['address'] = 'Address';
+        // $tblcustom_fields['pincode'] = 'Pincode';
+
         $tblcustom_fields = $this->customFields($request);
         $res['status_code'] = 1;
         $res['message'] = "Success";
@@ -41,15 +64,13 @@ class studentReportController extends Controller
 
     public function customFields(Request $request)
     {
-
-
         $sub_institute_id = $request->session()->get('sub_institute_id');
         $tblcustom_fields['enrollment_no'] = get_string('grno','request');
         // $tblcustom_fields['student_name'] = 'Student Name';
         $tblcustom_fields['first_name'] = 'First Name';
         $tblcustom_fields['middle_name'] = 'Middle Name';
         $tblcustom_fields['last_name'] = 'Surname';
-        $tblcustom_fields['studentmobile'] = get_string('studentmobile','request');
+        $tblcustom_fields['student_mobile'] = get_string('studentmobile','request');
         $tblcustom_fields['father_name'] = 'Father Name';
         $tblcustom_fields['mother_name'] = 'Mother Name';
         $tblcustom_fields['gender'] = 'Gender';
@@ -75,8 +96,9 @@ class studentReportController extends Controller
         $tblcustom_fields['image'] = 'Image';
         $tblcustom_fields['house'] = get_string('house','request');
         $tblcustom_fields['van'] = 'Van';
+        $tblcustom_fields['optional_subjects'] = 'Optional Subjects';        
         $tblcustom_fields['nationality'] = get_string('nationality','request');
-        $tblcustom_fields['birthplace'] = get_string('birthplace','request');
+        $tblcustom_fields['place_of_birth'] = get_string('birthplace','request');
 
         $tblcustoms = DB::table("tblcustom_fields")
             ->where(["status" => "1", "table_name" => "tblstudent"])
@@ -88,75 +110,7 @@ class studentReportController extends Controller
             $customfieldArray[$key] = $value;
         }
 
-
         return array_merge($tblcustom_fields, $customfieldArray);
-
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return void
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return void
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return void
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return void
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return void
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return void
-     */
-    public function destroy($id)
-    {
-        //
     }
 
     public function searchStudent(Request $request)
@@ -170,7 +124,7 @@ class studentReportController extends Controller
         $sub_institute_id = $request->session()->get('sub_institute_id');
         $syear = $request->session()->get('syear');
         $marking_period_id=session()->get('term_id');
-        
+
         $extra_order_by = '';
         $extraSearchArray = [];
         $extraSearchArray['tblstudent_enrollment.sub_institute_id'] = $sub_institute_id;
@@ -223,10 +177,10 @@ class studentReportController extends Controller
             // $res['message'] = "Please select one checkbox atlease to view report";
             // return is_mobile($type, "student_report.index", $res);
         } else {
-            $searchArr1 = ['enrollment_no', 'first_name', 'last_name'];
-            $replaceArr1 = [get_string('grno','request'), 'First Name', 'Surname'];
+            $searchArr1 = ['enrollment_no', 'first_name', 'last_name', 'place_of_birth', 'student_mobile','optional_subjects'];
+            $replaceArr1 = [get_string('grno','request'), 'First Name', 'Surname', get_string('birthplace','request'), get_string('studentmobile','request'),'Optional Subjects'];
             foreach ($request->input('dynamicFields') as $key => $value) {
-                if ($value != "bloodgroup" && $value != "van") {
+                if ($value != "bloodgroup" && $value != "van" && $value != "optional_subjects") {
                     $array[] = $value;
                 }
                 $value1 = str_replace($searchArr1, $replaceArr1, $value);
@@ -241,6 +195,9 @@ class studentReportController extends Controller
             $array[] = 'caste.caste_name as cast';
             $array[] = 'blood_group.bloodgroup as bloodgroup';
             $array[] = 'transport_vehicle.title as van';
+            $array[] = 'tblstudent.place_of_birth as place_of_birth';
+            $array[] = 'tblstudent.student_mobile as studentmobile';
+            $array[] = 'GROUP_CONCAT(IFNULL(subject.subject_name, "-")) as optional_subjects';            
         }
         $array[] = 'concat_ws(" ",tblstudent.first_name,tblstudent.middle_name,tblstudent.last_name) AS student_name';
 
@@ -249,9 +206,10 @@ class studentReportController extends Controller
             ->join('tblstudent_enrollment', 'tblstudent.id', '=', 'tblstudent_enrollment.student_id')
             ->join('academic_section', 'academic_section.id', '=', 'tblstudent_enrollment.grade_id')
             ->join('standard',function($join) use($marking_period_id){
-                $join->on( 'standard.id', '=', 'tblstudent_enrollment.standard_id')->when($marking_period_id,function($query) use($marking_period_id){
-                    $query->where('standard.marking_period_id',$marking_period_id);
-                });
+                $join->on( 'standard.id', '=', 'tblstudent_enrollment.standard_id');
+                // ->when($marking_period_id,function($query) use($marking_period_id){
+                //     $query->where('standard.marking_period_id',$marking_period_id);
+                // });
             })
             ->join('division', 'division.id', '=', 'tblstudent_enrollment.section_id')
             ->leftjoin('religion', 'religion.id', '=', 'tblstudent.religion')
@@ -261,9 +219,14 @@ class studentReportController extends Controller
             ->leftjoin('blood_group', 'blood_group.id', '=', 'tblstudent.bloodgroup')
             ->leftjoin('transport_map_student', 'transport_map_student.student_id', '=', 'tblstudent.id')
             ->leftjoin('transport_vehicle', 'transport_vehicle.id', '=', 'transport_map_student.from_bus_id')
+            ->leftjoin('student_optional_subject',function($join){
+                $join->on('student_optional_subject.student_id', '=', 'tblstudent.id')->where('student_optional_subject.syear',session()->get('syear')); 
+            })            
+            ->leftjoin('subject', 'student_optional_subject.subject_id', '=', 'subject.id')
             ->where($extraSearchArray)
             ->whereRaw('tblstudent_enrollment.end_date is NULL')
             ->orderByRaw($extra_order_by)
+            ->groupBy('tblstudent.id')
             ->get();
 
         $res['status_code'] = 1;
@@ -271,7 +234,7 @@ class studentReportController extends Controller
         $res['student_data'] = $student_data;
         $res['grade_id'] = $grade_id;
         $res['standard_id'] = $standard_id;
-        $res['division_id'] = $division_id;
+        $res['division_id'] = $division_id; 
         $res['data'] = $this->customFields($request);
         $res['headers'] = $header;
 
