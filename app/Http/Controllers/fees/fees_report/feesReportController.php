10838f953b5c7322290ffd66492c77384ef010b2
rp2164394 <rp2164394@gmail.com>
2023-07-21 11:51:29 +0530

    student report add student quota and batch

--- app/Http/Controllers/fees/fees_report/feesReportController.php
+++ app/Http/Controllers/fees/fees_report/feesReportController.php
@@ -95,15 +95,20 @@ class feesReportController extends Controller
         }
 
         $M = DB::table('tblstudent as t')
-            ->selectRaw("fp.student_id, t.enrollment_no, t.roll_no, t.uniqueid, CONCAT_WS(' ', t.first_name, t.middle_name, t.last_name) AS student_name, t.mobile, ac.title AS grade, s.name AS standard_name, d.name AS division_name, fp.created_date, CONCAT_WS(' ', u.first_name, u.last_name) AS user_name, fp.term_id, fp.receiptdate, fp.receipt_no, fp.payment_mode, fp.cheque_bank_name, fp.bank_branch, fp.cheque_no, fp.cheque_date, SUM(IFNULL(fp.amount, 0)) AS amount")
+            ->selectRaw("fp.student_id, t.enrollment_no, t.roll_no, t.uniqueid, CONCAT_WS(' ', t.first_name, t.middle_name, t.last_name) AS student_name, t.mobile, ac.title AS grade, s.name AS standard_name, d.name AS division_name, fp.created_date, CONCAT_WS(' ', u.first_name, u.last_name) AS user_name, fp.term_id, fp.receiptdate, fp.receipt_no, fp.payment_mode, fp.cheque_bank_name, fp.bank_branch, fp.cheque_no, fp.cheque_date, SUM(IFNULL(fp.amount, 0)) AS amount,b.title as batch,sq.title as quota")
             ->join('tblstudent_enrollment as te', 't.id', '=', 'te.student_id')
             ->join('academic_section as ac', 'ac.id', '=', 'te.grade_id')
             ->join('standard as s', function($join) use($marking_period_id){
-                $join->on('s.id', '=', 'te.standard_id')->when($marking_period_id,function($query) use ($marking_period_id){
-                    $query->where('s.marking_period_id',$marking_period_id);
-                });
+                $join->on('s.id', '=', 'te.standard_id');
+                // ->when($marking_period_id,function($query) use ($marking_period_id){
+                //     $query->where('s.marking_period_id',$marking_period_id);
+                // });
             })
             ->join('division as d', 'd.id', '=', 'te.section_id')
+            ->join('student_quota as sq', 'sq.id', '=', 'te.student_quota')            
+            ->leftjoin('batch as b',function($join) {
+                $join->on('b.standard_id', '=', 'te.standard_id')->whereRaw('b.division_id = te.section_id');
+            })  
             ->join('fees_collect as fp', 'fp.student_id', '=', 'te.student_id')
             ->leftJoin('tbluser as u', 'fp.created_by', '=', 'u.id')
             ->whereRaw("1=1 {$extra_fp}")
@@ -114,9 +119,10 @@ class feesReportController extends Controller
             ->join('tblstudent_enrollment as te', 't.id', '=', 'te.student_id')
             ->join('academic_section as ac', 'ac.id', '=', 'te.grade_id')
             ->join('standard as s', function($join) use($marking_period_id){
-                $join->on('s.id', '=', 'te.standard_id')->when($marking_period_id,function($query) use ($marking_period_id){
-                    $query->where('s.marking_period_id',$marking_period_id);
-                });
+                $join->on('s.id', '=', 'te.standard_id');
+                // ->when($marking_period_id,function($query) use ($marking_period_id){
+                //     $query->where('s.marking_period_id',$marking_period_id);
+                // });
             })
             ->join('division as d', 'd.id', '=', 'te.section_id')
             ->join('fees_paid_other as fo', 'fo.student_id', '=', 'te.student_id')
@@ -124,7 +130,7 @@ class feesReportController extends Controller
             ->groupBy('fo.student_id', 'fo.reciept_id', 'fo.syear', 'fo.receiptdate', 'fo.payment_mode', 'fo.cheque_dd_no');
 
         $query = DB::table(DB::raw("({$M->toSql()}) as M"))
-            ->selectRaw("M.student_id, M.enrollment_no, M.roll_no, M.uniqueid, M.student_name, M.mobile, M.grade, M.standard_name, M.division_name, M.created_date, M.user_name, M.term_id, M.receiptdate, M.receipt_no, M.payment_mode, M.cheque_bank_name, M.bank_branch, M.cheque_no, M.cheque_date, (IFNULL(M.amount, 0) + IFNULL(N.actual_amountpaid, 0)) AS actual_amountpaid")
+            ->selectRaw("M.student_id, M.enrollment_no, M.roll_no, M.uniqueid, M.student_name, M.mobile, M.grade, M.standard_name, M.division_name, M.created_date, M.user_name, M.term_id, M.receiptdate, M.receipt_no, M.payment_mode, M.cheque_bank_name, M.bank_branch, M.cheque_no, M.cheque_date, (IFNULL(M.amount, 0) + IFNULL(N.actual_amountpaid, 0)) AS actual_amountpaid,M.batch,M.quota")
             ->leftJoin(DB::raw("({$N->toSql()}) as N"), function ($join) {
                 $join->on('M.student_id', '=', 'N.student_id')
                     ->on('M.receipt_no', '=', 'N.reciept_id');
@@ -137,7 +143,7 @@ class feesReportController extends Controller
         $feesData = $query->get()->toArray();
 
         $feesData = json_decode(json_encode($feesData), true);
-
+        // echo "<pre>";print_r($feesData);exit;
         $res['status_code'] = 1;
         $res['message'] = "Success";
         $res['fees_data'] = $feesData;
