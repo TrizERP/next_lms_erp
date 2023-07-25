318b64c1026e5f5115cb28c081f2d71ebaff2550
rp2164394 <rp2164394@gmail.com>
2023-07-25 14:41:41 +0530

    Result marks excel download changes

--- resources/views/import/excel_download/show.blade.php
+++ resources/views/import/excel_download/show.blade.php
@@ -1,104 +1,151 @@
-@include('includes.lmsheadcss')
-@include('includes.header')
-@include('includes.sideNavigation')
-
+@include('../includes.headcss')
+@include('../includes.header')
+@include('../includes.sideNavigation')
 
 <div id="page-wrapper">
-    <div class="container-fluid">
-        <div class="row bg-title">
-            <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h4 class="page-title">Result Marks Excel</h4>
-            </div>
-        </div>
-        @php
-            $grade_id = $standard_id = $division_id = '';
-
-            if(isset($data['grade_id'])){
-                $grade_id = $data['grade_id'];
-                $standard_id = $data['standard_id'];
-                $division_id = $data['division_id'];
-            }
-
-        @endphp
+    <div class="container-fluid">       
             <div class="card">
-                <div class="card-body">
-                    @if ($sessionData = Session::get('data'))
-                        <div class="alert alert-success alert-block">
-                            <button type="button" class="close" data-dismiss="alert">×</button>
-                            <strong>{{ $sessionData['message'] }}</strong>
-                        </div>
-                    @endif
-                    <form action="{{ route('result-marks-excel.create') }}">
-                        @csrf
+                @if(!empty($data['message']))
+                <div class="alert alert-{{ $data['class'] }} alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>{{ $data['message'] }}</strong>
+                </div>
+                @endif
+                <div class="col-lg-12 col-sm-12 col-xs-12">
+                    <form action="{{ route('result-marks-excel.create') }}" enctype="multipart/form-data">
+                        {{csrf_field()}}
                         <div class="row">
-                            {{ App\Helpers\SearchChain('3','required','grade,std,div',$grade_id,$standard_id,$division_id) }}
-                            <div class="col-md-3 form-group">
-                                <label for="subject">Select Subject</label>
-                                <select name="subject" id="subject" class="cust-select form-control mb-0">
-                                    @if(empty($data['subject_data']))
-                                        <option value="">Select Subject</option>
-                                    @endif
-                                    @if(!empty($data['subject_data']))
-                                        @foreach($data['subject_data'] as $k1 => $v1)
-                                            <option
-                                                value="{{$v1['subject_id']}}" @if(isset($data['subject_id'])){{$data['subject_id'] == $v1['subject_id'] ? 'selected' : '' }} @endif>{{$v1['display_name']}} </option>
-                                        @endforeach
-                                    @endif
+                            {{ App\Helpers\TermDD() }}
+                        
+                            {{ App\Helpers\SearchChain('4','single','grade,std,div') }}
+                        
+
+                            <div class="col-md-4 form-group">
+                                <label for="title">Select Subject:</label>
+                                <select name="subject" id="subject" class="form-control">
+                                    <option value="">Select</option>
                                 </select>
                             </div>
-                            <div class="col-md-3 form-group">
-                                <label for="exam">Select Exam</label>
-                                <select class="cust-select form-control mb-0" name="exam_id[]" multiple="multiple"
-                                        required="required">
-                                    @if(!empty($data['exams_data']))
-                                        @foreach($data['exams_data'] as $k => $v)
-                                            <option
-                                                value="{{$v->id}}" @if(isset($data->exam_id)){{$data->exam_id == $v->id ? 'selected=selected' : '' }} @endif>{{$v->paper_name}}</option>
-                                        @endforeach
-                                    @endif
+
+                            <div class="col-md-4 form-group">
+                                <label for="title">Select Exam:</label>
+                                <select name="exam_id[]" id="exam" class="form-control" multiple>
+                                    <option value="">Select</option>
                                 </select>
                             </div>
-                            <div class="col-md-2 form-group mt-4">
-                                <br>
-                                <input type="submit" name="submit" value="Download Excel" class="btn btn-success">
+
+
+                            <div class="col-md-12 form-group">
+                                <center>
+                                    <input type="submit" name="submit" value="Download Excel" class="btn btn-success" >
+                                </center>
                             </div>
                         </div>
                     </form>
                 </div>
-            </div>
-            </div>
-       
-@include('includes.lmsfooterJs')
+                @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+            </div>       
+    </div>
+</div>
+
+@include('includes.footerJs')
 <script>
-    $("#standard").change(function () {
-        var std_id = $("#standard").val();
-        var path = "{{ route('ajax_LMS_StandardwiseSubject') }}";
-        $('#subject').find('option').remove().end().append('<option value="">Select Subject</option>').val('');
-        $.ajax({
-            url: path, data: 'std_id=' + std_id, success: function (result) {
-                for (var i = 0; i < result.length; i++) {
-                    $("#subject").append($("<option></option>").val(result[i]['subject_id']).html(result[i]['display_name']));
-                }
-            }
-        });
-    })
+    $("#grade").prop('required', true);
+    $("#standard").prop('required', true);
+    $("#division").prop('required', true);
+    $("#subject").prop('required', true);
+    $("#term").prop('required', true);
+    $("#exam").prop('required', true);
+    $('#term').change(function () {
+        $("#grade").val("");
+        $("#standard").empty();
+        $("#standard").append('<option value="">Select</option>');
+        $("#division").empty();
+        $("#division").append('<option value="">Select</option>');
+        $("#subject").empty();
+        $("#subject").append('<option value="">Select</option>');
+        $("#exam").empty();
+        $("#exam").append('<option value="">Select</option>');
+    });
+    $('#grade').change(function () {
+        $("#subject").empty();
+        $("#subject").append('<option value="">Select</option>');
+        $("#exam").empty();
+        $("#exam").append('<option value="">Select</option>');
+    });
+    $('#standard').change(function () {
+        $("#subject").empty();
+        $("#subject").append('<option value="">Select</option>');
+        $("#exam").empty();
+        $("#exam").append('<option value="">Select</option>');
+    });
+    $('#division').on('change', function () {
+        $("#exam").empty();
+        $("#exam").append('<option value="">Select</option>');
+        var standardID = $("#standard").val();
+        var divisionID = $("#division").val();
+        if (standardID) {
+            $.ajax({
+                type: "GET",
+                url: "/api/get-subject-list?standard_id=" + standardID + "&division_id="+ divisionID,
+                success: function (res) {
+                    if (res) {
+                        $("#subject").empty();
+                        $("#subject").append('<option value="">Select</option>');
+                        $.each(res, function (key, value) {
+                            $("#subject").append('<option value="' + key + '">' + value + '</option>');
+                        });
 
-    $("#subject").change(function(){
-        var std_id = $("#standard").val();
-        var sub_id = $("#subject").val();
-        var path = "{{ route('ajax_LMS_SubjectWiseExam') }}";
-        $.ajax({
-            url: path,
-            data: 'std_id=' + std_id + '&sub_id=' + sub_id,
-            success: function (result) {
-                var e = $('select[name="exam_id[]"]');
-                $(e).find('option').remove().end();
-                for (var i = 0; i < result.length; i++) {
-                    $(e).append($("<option></option>").val(result[i]['id']).html(result[i]['paper_name']));
+                    } else {
+                        $("#subject").empty();
+                    }
                 }
+            });
+        } else {
+            $("#subject").empty();
+        }
+
+    });
+    $('#subject').on('change', function () {
+        var standardID = $("#standard").val();
+        var subjectID = $("#subject").val();
+        var termID = $("#term").val();
+
+        if (standardID && subjectID && termID) {
+            $.ajax({
+                type: "GET",
+                url: "/api/get-exam-list?standard_id=" + standardID +
+                        "&subject_id=" + subjectID + "&term_id=" + termID,
+                success: function (res) {
+                    if (res) {
+                        $("#exam").empty();
+                        $("#exam").append('<option value="">Select</option>');
+                        $.each(res, function (key, value) {
+                            $("#exam").append('<option value="' + key + '">' + value + '</option>');
+                        });
+
+                    } else {
+                        $("#exam").empty();
+                    }
+                }
+            });
+        } else {
+            $("#exam").empty();
+            $("#exam").append('<option value="">Select</option>');
+            if (termID == "") {
+                alert("Please Select Term.");
             }
-        });
-    })
+        }
+
+    });
 </script>
-
 @include('includes.footer')
