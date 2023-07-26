03c7f32f0f5f39cf842bbb8005240d2f12bfc6a0
Rajesh <rajeshrafaliya@gmail.com>
2023-07-26 12:35:08 +0530

    Van wise Add GR column

--- app/Http/Controllers/transportation/van_wise_report/van_wise_report_controller.php
+++ app/Http/Controllers/transportation/van_wise_report/van_wise_report_controller.php
@@ -153,7 +153,7 @@ class van_wise_report_controller extends Controller
                 $join->whereRaw("cd.id = tv.conductor");
             })
             ->selectRaw("ts.id AS student_id,CONCAT_WS(' ',ts.first_name,ts.middle_name,ts.last_name) name, 
-    concat(s.name,'/',d.name) as stddiv,ts.mobile,ts.address, tr.route_name, tv.title as bus_name, st.stop_name,
+    concat(s.name,'/',d.name) as stddiv,ts.mobile,ts.enrollment_no,ts.address, tr.route_name, tv.title as bus_name, st.stop_name,
     dd.first_name driver, cd.first_name conductor")
             ->whereRaw($where)
             ->groupBy('student_id')
