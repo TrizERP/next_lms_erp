fc6f50fe2a15dea3e8bf6ebf365685865212e839
minhazkhan60 <minhazkhan60@gmail.com>
2023-08-23 13:04:18 +0530

    bonofied certi & notification

--- app/Http/Controllers/student/studentCertificateController.php
+++ app/Http/Controllers/student/studentCertificateController.php
@@ -408,6 +408,7 @@ class studentCertificateController extends Controller
         $sub_institute_id = $request->session()->get('sub_institute_id');
         $template = $request->input('template');
         $student_ids = explode(',', $student_id);
+        $certificate_reason = "";
 
         $data = getStudents($student_ids);
 
@@ -444,7 +445,7 @@ class studentCertificateController extends Controller
 
             $html_content = $tData[0]['html_content'];
             $new_html_content = $this->create_html_content($syear, $sub_institute_id, $html_content, $value,
-                $receipt_book_arr, $template, $certificate_no);
+                $receipt_book_arr, $template, $certificate_no, $certificate_reason);
             DB::table('certificate_history')->insert([
                 'syear'              => $syear,
                 'student_id'         => $value['id'],
