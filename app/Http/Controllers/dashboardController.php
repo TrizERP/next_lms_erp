b08d02e7a976a8b6a6be056c83c58f635ca3cc9b
Rajesh <rajesh.rafaliya@triz.co.in>
2023-09-13 11:14:03 +0530

    live 13-09-2023

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -1212,7 +1212,26 @@ class dashboardController extends Controller
                 ->orderByRaw("DATE_FORMAT(s.birthdate, '%m-%d')")
                 ->get()->toArray();
 
-
+  $studentLeaves = DB::table("leave_applications as l")
+                    ->join("tblstudent as s", function ($join) {
+                        $join->whereRaw("l.student_id = s.id");
+                    })
+                    ->join("tblstudent_enrollment as se", function ($join) use ($syear) {
+                        $join->whereRaw("s.id = se.student_id AND se.syear = " . $syear);
+                    })
+                    ->join("standard as st", function ($join) {
+                        $join->whereRaw("st.id = se.standard_id");
+                    })
+                    ->join("division as dt", function ($join) {
+                        $join->whereRaw("dt.id = se.section_id");
+                    })
+                    ->selectRaw("l.*, CONCAT_WS(' ',s.first_name,s.middle_name,s.last_name) AS student_name,st.name AS standard_name,
+            dt.name AS division_name")
+                    ->where("l.sub_institute_id", "=", $sub_institute_id)
+                    ->where("l.apply_date", "=", $date)
+                    ->limit(10)
+                    ->get()->toArray();
+                    
             $calendarEvents = DB::table('calendar_events')->where('sub_institute_id', $sub_institute_id)
                 ->where('school_date', ">=", $date)->where('school_date', "<=", $date15)->get()->toArray();
 
--- app/Http/Controllers/result/ExamMaster/ExamMasterController.php
+++ app/Http/Controllers/result/ExamMaster/ExamMasterController.php
@@ -68,7 +68,7 @@ class ExamMasterController extends Controller
                     'Code' => $request->get('Code'),
                     'ExamType' => 14,
                     'ExamTitle' => $request->get('ExamTitle'),
-                    'SortOrder' => $sort++,
+                    'SortOrder' => $request->get('SortOrder'),//$sort++,
                     'SubInstituteId' => session()->get('sub_institute_id'),
                     'created_at' => now(),
                     'standard_id' => $std,
@@ -100,14 +100,17 @@ class ExamMasterController extends Controller
         $exam = ExamMaster::select('result_exam_master.*',
             DB::raw('COUNT(result_create_exam.id) AS total_count'),'standard.name as std_name','academic_year.title as term')
             ->leftjoin('standard', 'standard.id', '=', 'result_exam_master.standard_id')
-            ->leftjoin('academic_year', 'academic_year.term_id', '=', 'result_exam_master.term_id')            
+            ->leftJoin('academic_year', function ($join) {
+                $join->on('academic_year.term_id', '=', 'result_exam_master.term_id')
+                     ->on('academic_year.sub_institute_id', '=', 'result_exam_master.SubInstituteId');
+            })
             ->leftjoin("result_create_exam", function ($join) {
                 $join->on("result_create_exam.exam_id", "=", "result_exam_master.Id")
                     ->on("result_create_exam.sub_institute_id", "=", "result_exam_master.SubInstituteId");
             })
             ->where(['result_exam_master.SubInstituteId' => $sub_institute_id])
             ->groupby('result_exam_master.Id')
-            ->orderByRaw('result_exam_master.SortOrder,standard.name,academic_year.title')            
+            ->orderByRaw('standard.sort_order,academic_year.sort_order,result_exam_master.SortOrder')            
             ->get();
         $i = 1;
         foreach ($exam as $id => $arr) {
@@ -140,15 +143,14 @@ class ExamMasterController extends Controller
 
     public function update(Request $request, $id)
     {
+        //dd($request);
         $data = [
-            'Code' => $request->get('Code'),
-            'ExamType' => 14,
             'ExamTitle' => $request->get('ExamTitle'),
             'SortOrder' => $request->get('SortOrder'),
             'SubInstituteId' => session()->get('sub_institute_id'),
             'created_at' => now(),
-            'standard_id' => $std,
-            'term_id' => $term,
+            'standard_id' => $request->input('all_standard.0'),
+            'term_id' => $request->input('all_term.0'),
             'weightage' => $request->get('weightage') ?? '',
             'created_by' => session()->get('user_id'),
         ];
