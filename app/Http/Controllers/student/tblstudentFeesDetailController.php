3bbd9980727c45140dfd8b5e6827918b1a4e7f9d
Rajesh <rajesh.rafaliya@triz.co.in>
2023-09-18 12:40:07 +0530

    live 18-09-2023

--- app/Http/Controllers/student/tblstudentFeesDetailController.php
+++ app/Http/Controllers/student/tblstudentFeesDetailController.php
@@ -75,11 +75,9 @@ class tblstudentFeesDetailController extends Controller
                 //BACKUP Old record and update new Account Number
                 $is_registered = 'N';
                 $insLogSql = "INSERT INTO tblstudent_bank_detail_log
-                (student_id, ac_holder_name, ac_number, bank_name, bank_branch, ifsc_code, is_registered, created_by, AC_TYPE,UMRN,closure_date,status,reason,
-                    created_on,registration_date)
+                (student_id, ac_holder_name, ac_number, bank_name, bank_branch, ifsc_code, is_registered, created_by, AC_TYPE,UMRN,closure_date,status,reason,created_on,registration_date)
                 SELECT student_id, ac_holder_name, ac_number, bank_name, bank_branch, ifsc_code, is_registered, '".$user_id."',
-                    AC_TYPE,UMRN,closure_date,status,reason, created_on ,
-                registration_date
+                    AC_TYPE,UMRN,closure_date,status,reason, created_on,registration_date
                 FROM tblstudent_bank_detail WHERE student_id = '".$request->get('student_id')."'";
                 DB::select($insLogSql);
             }
