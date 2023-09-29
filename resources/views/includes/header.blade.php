c9da7f4893e2c6ef676ae6b684fd7b82c05b6da3
minhazkhan60 <minhazkhan60@gmail.com>
2023-09-29 16:54:52 +0530

    online fees continue & onboarding link and menu

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -161,6 +161,8 @@ $academicTerms = session()->get('academicTerms');
                                     class="mdi mdi-content-save-settings-outline"></i> ERP Status</a>
                             <a class="dropdown-item" href="{{route('implementation')}}"><i
                                     class="mdi mdi-checkerboard"></i> Implementation</a>
+                            <a class="dropdown-item" href="{{route('Onboarding')}}"><i
+                                    class="mdi mdi-checkerboard"></i> Onboarding</a>
                             @if(Session::get('user_profile_name') == 'Admin')
                                 <a class="dropdown-item" href="{{route('norm-clature.index')}}"><i
                                         class="mdi mdi-wallet-travel"></i> Language Setting</a>
