f615f7540ca93923a8ba28392cef174bc85cbc7f
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2023-11-02 17:16:22 +0530

    Merge pull request #41 from TrizERP/development_rp
    fix add attachment and youtube_link in add_adminPhotoVedioAPI

--- app/Http/Controllers/front_desk/photo_video_gallary/photo_video_gallaryController.php
+++ app/Http/Controllers/front_desk/photo_video_gallary/photo_video_gallaryController.php
@@ -234,8 +234,10 @@ class photo_video_gallaryController extends Controller
             $sub_institute_id = session()->get('sub_institute_id');
             $user_id = session()->get('user_id');
         }
+        $status = 1;
 
         if ($_REQUEST['type'] == 'Photo') {
+            // return "photo";exit;
             if ($request->hasFile('attachment')) {
                 foreach ($request->file('attachment') as $key => $file_data) {
                     $file_name = $file_size = $ext = "";
@@ -264,6 +266,7 @@ class photo_video_gallaryController extends Controller
                                     'created_at'       => now(),
                                     'updated_at'       => now(),
                                 ];
+                                
                                 DB::table('photo_video_gallary')->insert($values);
 
                                 //START Send Notification Code
@@ -353,12 +356,19 @@ class photo_video_gallaryController extends Controller
                         }
                     }
                 }
+            }else{
+                $status = 0;
             }
         } else {
             if (isset($_REQUEST['standard'])) {
                 foreach ($_REQUEST['standard'] as $id => $std) {
                     foreach ($_REQUEST['division'] as $ids => $div_id) {
-                        $file_name = $_REQUEST['attachment'];
+                        $file_name="";
+                        if(isset($_REQUEST['attachment'])){
+                            $file_name = $_REQUEST['attachment'];
+                        }else if($_REQUEST['youtube_link']){
+                            $file_name = $_REQUEST['youtube_link'];
+                        }
                         $values = [
                             'syear'            => $syear,
                             'standard_id'      => $std,
@@ -372,7 +382,21 @@ class photo_video_gallaryController extends Controller
                             'created_at'       => now(),
                             'updated_at'       => now(),
                         ];
-                        DB::table('photo_video_gallary')->insert($values);
+                        $check = DB::table('photo_video_gallary')->where([
+                            'syear'            => $syear,
+                            'standard_id'      => $std,
+                            'division_id'      => $div_id,
+                            'title'            => $_REQUEST['title'],
+                            'album_title'      => $_REQUEST['album_title'],
+                            'type'             => $_REQUEST['type'],
+                            'file_name'        => $file_name,
+                            'date_'            => $_REQUEST['date_'],
+                            'sub_institute_id' => $sub_institute_id,
+                        ])->get()->toArray();
+
+                        if(empty($check)){
+                            DB::table('photo_video_gallary')->insert($values);
+                        }
 
                         //START Send Notification Code
                         $student_data = DB::table("tblstudent_enrollment as se")
@@ -461,7 +485,7 @@ class photo_video_gallaryController extends Controller
         }
 
         if (isset($_REQUEST['action']) && $_REQUEST['action'] == "API") {
-            return 1;
+            return $status;
         } else {
             $res = [
                 "status"  => 1,
