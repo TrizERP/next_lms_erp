45c22ca86faa536669981f7c03fb88cc7cb00f5a
Rajesh <rajesh.rafaliya@triz.co.in>
2023-11-04 16:45:58 +0530

    issue: live_2023-11-04

--- app/Http/Controllers/AJAXController.php
+++ app/Http/Controllers/AJAXController.php
@@ -1545,6 +1545,7 @@ class AJAXController extends Controller
         ->where('fc.sub_institute_id', $sub_institute_id)
         ->where('fc.syear', $syear)
         ->where('fc.student_id', $student_id)
+        ->where('fc.receipt_no', $receipt_id)
         ->whereRaw("(fr.RECEIPT_ID_1 = '" . $receipt_id . "' OR fr.RECEIPT_ID_2 = '" . $receipt_id . "' OR fr.RECEIPT_ID_3 = '" . $receipt_id . "'
             OR fr.RECEIPT_ID_4 = '" . $receipt_id . "' OR fr.RECEIPT_ID_5 = '" . $receipt_id . "' OR fr.RECEIPT_ID_6 = '" . $receipt_id . "' OR fr.RECEIPT_ID_7 = '" . $receipt_id . "' OR fr.RECEIPT_ID_8 = '" . $receipt_id . "'
             OR fr.RECEIPT_ID_9 = '" . $receipt_id . "' OR fr.RECEIPT_ID_10 = '" . $receipt_id . "')")
@@ -1558,6 +1559,7 @@ class AJAXController extends Controller
                 ->where('fo.sub_institute_id', $sub_institute_id)
                 ->where('fo.syear', $syear)
                 ->where('fo.student_id', $student_id)
+                ->where('fo.reciept_id', $receipt_id)
                 ->whereRaw("(fro.RECEIPT_ID_1 = '" . $receipt_id . "' OR fro.RECEIPT_ID_2 = '" . $receipt_id . "' OR fro.RECEIPT_ID_3 = '" . $receipt_id . "' OR fro.RECEIPT_ID_4 = '" . $receipt_id . "' OR fro.RECEIPT_ID_5 = '" . $receipt_id . "' OR fro.RECEIPT_ID_6 = '" . $receipt_id . "' OR fro.RECEIPT_ID_7 = '" . $receipt_id . "' OR fro.RECEIPT_ID_8 = '" . $receipt_id . "'
                     OR fro.RECEIPT_ID_9 = '" . $receipt_id . "' OR fro.RECEIPT_ID_10 = '" . $receipt_id . "')")
                 ->groupBy('fo.paid_fees_html')
