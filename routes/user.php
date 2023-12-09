ae89a90b6d1c3cb3b29573510fe9bb1d2c6c99f3
minhazkhan60 <minhazkhan60@gmail.com>
2023-12-09 13:59:41 +0530

    user profile wise menu rights

--- routes/user.php
+++ routes/user.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\user\tbluserController;
 use App\Http\Controllers\user\tbluserPastEducationController;
 use App\Http\Controllers\user\tbluserprofilemasterController;
 use App\Http\Controllers\user\userReportController;
+use App\Http\Controllers\user\tbluserProfileWiseMenuController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -17,6 +18,8 @@ Route::group(['prefix' => 'user', 'middleware' => ['session', 'menu', 'logRoute'
     Route::resource('add_mobileapp_menu_rights', mobileapp_menu_rightsController::class);
     Route::resource('add_user_past_education', tbluserPastEducationController::class);
     Route::resource('user_report', userReportController::Class);
+    Route::resource('user_profile_wise_menu_rights', tbluserProfileWiseMenuController::class);
+
     Route::post('show_user_report', [userReportController::class, 'searchUser'])->name("show_user_report");
      Route::post('ajax_userProfile_Data_Create',
         [tblgroupwise_rightsController::class, 'ajax_userProfile_Data_Create'])->name('ajax_userProfile_Data_Create');
@@ -29,6 +32,8 @@ Route::group(['prefix' => 'user', 'middleware' => ['session', 'menu', 'logRoute'
         [tblindividual_rightsController::Class, 'profileWiseUsers'])->name('ajax_profileWiseUsers');
     Route::get('ajax_individualrights',
         [tblindividual_rightsController::class, 'displayIndividualRights'])->name('ajax_individualrights');
+    Route::get('ajax_user_profile_wise_rights',
+        [tbluserProfileWiseMenuController::class, 'displayUserProfileWiseRights'])->name('ajax_user_profile_wise_rights');
 });
 
 Route::post('/teacherListAPI', [tbluserController::class, 'teacherListAPI']);
