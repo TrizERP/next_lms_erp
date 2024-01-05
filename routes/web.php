570ec891a29604c70ee50a1f2dc08886b763d657
minhazkhan60 <minhazkhan60@gmail.com>
2024-01-05 17:37:15 +0530

    HRMS:attendance report

--- routes/web.php
+++ routes/web.php
@@ -165,6 +165,7 @@ Route::group([ 'middleware' => ['session', 'menu', 'logRoute']], function () {
 
     Route::get('hrms-attendance-report',[HrmsController::Class,'hrmsAttendanceReport'])->name('hrms_attendance_report.index');
     Route::post('/hrms-attendance-report', [HrmsController::class, 'hrmsAttendanceReport'])->name('hrms.show_hrms_attendance_report');
+    Route::post('/get-employees-list', [HrmsController::class, 'getEmployeeLists'])->name('get-employees-list');
 
     Route::get('early-going-hrms-attendance-report',[HrmsController::Class,'earlyGoingHrmsAttendanceReport'])->name('hrms_attendance_report.early_going_report');
     Route::post('/early-going-hrms-attendance-report', [HrmsController::class, 'earlyGoingHrmsAttendanceReport'])->name('hrms.show_early_going_hrms_attendance_report');
