bbf3bca403644fdf863ebe1e3b4ad2852cd19911
minhazkhan60 <minhazkhan60@gmail.com>
2024-01-11 15:03:13 +0530

    HRMS:attendance_report->error slove

--- resources/views/HRMS/hrms_attendance_report/index.blade.php
+++ resources/views/HRMS/hrms_attendance_report/index.blade.php
@@ -234,11 +234,81 @@
 </div>
 
 @include('includes.footerJs')
+<<<<<<< HEAD
+
+<script>
+    $(document).ready(function () {
+        var table = $('#example').DataTable({
+            ordering: false,
+            select: true,
+            lengthMenu: [
+                [100, 500, 1000, -1],
+                ->100', '500', '1000', 'Show All
+            ],
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'pdfHtml5',
+                    title: 'Student Report',
+                    orientation: 'landscape',
+                    pageSize: 'LEGAL',
+                    pageSize: 'A0',
+                    exportOptions: {
+                        columns: ':visible'
+                    },
+                },
+                {extend: 'csv', text: ' CSV', title: 'Student Report'},
+                {extend: 'excel', text: ' EXCEL', title: 'Student Report'},
+                {extend: 'print', text: ' PRINT', title: 'Student Report'},
+                'pageLength'
+            ],
+        });
+        //table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+
+        $('#example thead tr').clone(true).appendTo('#example thead');
+        $('#example thead tr:eq(1) th').each(function (i) {
+            var title = $(this).text();
+            $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+
+            $('input', this).on('keyup change', function () {
+                if (table.column(i).search() !== this.value) {
+                    table
+                        .column(i)
+                        .search(this.value)
+                        .draw();
+                }
+            });
+        });
+        
+        // Ajax call to get employees based on the selected department
+        $(document).on("change", "#department_id", function(e) {
+            $('#employee_id').empty();
+            var departmentId = $(this).val();
+            
+            $.ajax({
+                type: "post",
+                url: "{{ route('get.employees.list') }}",
+                data: { department_id: departmentId },
+                success: function(data) {
+                    var options = '<option value="">Select Employee</option>';
+                    $.each(data.employees, function(index, employee) {
+                        // var selected = (employee.id == {{ json_encode($employee_id) }}) ? 'selected' : '';
+                        options += '<option value="' + employee.id + '" >' + employee.first_name + ' ' + employee.last_name + '</option>';
+                    });
+                    $('#employee_id').html(options);
+                },
+                error: function(xhr) {
+                    console.error(xhr.responseText);
+                }
+            });
+        });
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
         $('[data-toggle="tooltip"]').tooltip();
+
     });
 </script>
 <script>
