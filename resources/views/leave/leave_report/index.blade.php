a75b7797964c195ef4178110fd3f412a2b9a223f
minhazkhan60 <minhazkhan60@gmail.com>
2024-01-12 12:48:44 +0530

    changes:leave->leave_report/summary_report/web/attendance

--- resources/views/leave/leave_report/index.blade.php
+++ resources/views/leave/leave_report/index.blade.php
@@ -16,7 +16,7 @@
                     @else
                         <div class="alert alert-danger alert-block">
                     @endif
-                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <button type="button" class="close" data-dismiss="alert">Ã—</button>
                         <strong>{{ $sessionData->message }}</strong>
                     </div>
                 @endif
@@ -40,9 +40,13 @@
                         </div>
                         <div class="col-md-3 form-group">
                             <label>Employee List</label>
-                            <select id='employee_id' name="employee_id" class="form-control">
+                            <select id='employee_id' name="employee_id" class="form-control" required>
                                 <option value="">Select Employee</option>
-                                
+                                @if(!empty($employees))
+                                    @foreach($employees as $key=>$value)
+                                        <option value="{{$value['id']}}" @if(isset($employee_id) && $employee_id == $value['id']) selected @endif>{{$value['first_name'] ?? ''}} {{$value['last_name'] ?? ''}}</option>
+                                    @endforeach
+                                @endif
                             </select>
                         </div>
                         <div class="col-md-3 form-group">
@@ -200,28 +204,27 @@
                 }
             });
         });
+    });
+</script>
+<script>
+    $(document).on("change", "#department_id", function(e) {
+        $('#employee_id').empty();
+        var departmentId = $(this).val();
         
-        // Ajax call to get employees based on the selected department
-        $(document).on("change", "#department_id", function(e) {
-            $('#employee_id').empty();
-            var departmentId = $(this).val();
-            
-            $.ajax({
-                type: "post",
-                url: "{{ route('get-emp-list') }}",
-                data: { department_id: departmentId },
-                success: function(data) {
-                    var options = '<option value="">Select Employee</option>';
-                    $.each(data.employees, function(index, employee) {
-                        // var selected = (employee.id == {{ json_encode($employee_id) }}) ? 'selected' : '';
-                        options += '<option value="' + employee.id + '" >' + employee.first_name + ' ' + employee.last_name + '</option>';
-                    });
-                    $('#employee_id').html(options);
-                },
-                error: function(xhr) {
-                    console.error(xhr.responseText);
-                }
-            });
+        $.ajax({
+            type: "post",
+            url: "{{ route('get-emp-list') }}",
+            data: { department_id: departmentId },
+            success: function(data) {
+                var options = '';
+                $.each(data.employees, function(index, employee) {
+                    options += '<option value="' + employee.id + '" >' + employee.first_name + ' ' + employee.last_name + '</option>';
+                });
+                $('#employee_id').append(options);
+            },
+            error: function(xhr) {
+                console.error(xhr.responseText);
+            }
         });
     });
 </script>
