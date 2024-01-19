462996a3bb58489c302a6daee7d5cc4e0be7d260
Rajesh Triz <rajesh.rafaliya@triz.co.in>
2024-01-19 16:09:16 +0530

    dev-19-01-2024

--- app/Http/Controllers/AJAXController.php
+++ app/Http/Controllers/AJAXController.php
@@ -702,14 +702,14 @@ class AJAXController extends Controller
                     </tr>';
         foreach ($full_bk as $id => $val) {
             if($val!=0){
-                $id_attr="";
+                $ids ='';
                 if($id=="Total"){
-                    $id_attr ="id='all_total'";
+                    $ids='id="all_total"';
                 }
             $response .= "
                  <tr>
                     <td style='width: 20%'>$id</td>
-                    <td style='width: 20%' $id_attr>$val</td>
+                    <td style='width: 20%' $ids>$val</td>
             ";
             if ($id != 'Total') {
                 // $response .= "<td style='width: 20%'><input type='number' min=0 max=$val  value='" . $val . "' name='fees_data[" . $final_bk_name[$id] . "]' class='form-control allField1'></td>";
