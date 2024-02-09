107ab40b64ba4150a59d77c3c6003b1d6962d403
minhazkhan60 <minhazkhan60@gmail.com>
2024-02-09 17:04:28 +0530

    Hills:add filed amount van wise report

--- app/Http/Controllers/transportation/van_wise_report/van_wise_report_controller.php
+++ app/Http/Controllers/transportation/van_wise_report/van_wise_report_controller.php
@@ -158,7 +158,7 @@ class van_wise_report_controller extends Controller
             })
             ->selectRaw("ts.id AS student_id,CONCAT_WS(' ',ts.first_name,ts.middle_name,ts.last_name) name, 
     concat(s.name,'/',d.name) as stddiv,ts.mobile,ts.enrollment_no,ts.address, tr.route_name, tv.title as bus_name, st.stop_name,
-    dd.first_name driver, cd.first_name conductor")
+    dd.first_name driver, cd.first_name conductor, tm.amount as van_vise_amount")
             ->whereRaw($where)
             ->groupBy('student_id')
             ->get()->toarray();
