fbe958107e5b77690006e1f5b7a5f9c703147dbd
rp2164394 <rp2164394@gmail.com>
2024-03-30 13:31:22 +0530

    PAL Test module static 10 questions

---
+++ database/migrations/2024_03_30_123353_create_lms_online_exam_answer_student_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lms_online_exam_answer_student', function (Blueprint $table) {
+            $table->comment('');
+            $table->bigIncrements('id');
+            $table->integer('question_paper_id')->nullable();
+            $table->integer('online_exam_id')->nullable();
+            $table->integer('student_id')->nullable();
+            $table->integer('question_id')->nullable();
+            $table->integer('answer_id')->nullable();         
+            $table->longText('narrative_answer')->nullable();      
+            $table->string('ans_status',50)->nullable();                                                            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lms_online_exam_answer_student');
+    }
+};
