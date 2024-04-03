ce9a1365b1c826b845c2ff3ed90bfc09d3394a27
rp2164394 <rp2164394@gmail.com>
2024-04-03 13:58:20 +0530

    datatables print date and time at the end

--- resources/views/student/show_student_report.blade.php
+++ resources/views/student/show_student_report.blade.php
@@ -239,6 +239,7 @@ function checkedAll() {
                     title: 'Student Report',
                     customize: function (win) {
                         $(win.document.body).prepend(`{!! App\Helpers\get_school_details("$grade_id", "$standard_id", "$division_id") !!}`);
+                        $(win.document.body).append(`<div style="text-align: right;margin-top:20px">Printed on: {{date('d-m-Y H:i:s')}}</div>`);
                     }
                 },
                 'pageLength'
