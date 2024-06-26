1322e34db346e87a331280cc436e63720cd8affe
robertparker99 <rp2164394@gmail.com>
2024-06-26 14:35:02 +0530

    multiple department select in employee leave report

--- resources/views/leave/leave_report/index.blade.php
+++ resources/views/leave/leave_report/index.blade.php
@@ -23,32 +23,18 @@
                 <form action="{{route('leave.report.show')}}" enctype="multipart/form-data" method="post">
                 @csrf
                     <div class="row">
-                        <div class="col-md-3 form-group">
-                            <label>Department List</label>
-                            <select id='department_id' name="department_id" class="form-control" required>
-                                <option value="">Select Department</option>
-                                @foreach($data['departments'] as $id => $department)
-                                    <option value="{{$id}}"
-                                    @if(isset($data['department_id']))
-                                        @if($data['department_id'] == $id)
-                                        selected='selected'
-                                        @endif
-                                    @endif
-                                    >{{ $department }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-3 form-group">
-                            <label>Employee List</label>
-                            <select id='employee_id' name="employee_id" class="form-control" required>
-                                <option value="">Select Employee</option>
-                                @if(!empty($data['employees']))
-                                    @foreach($data['employees'] as $key=>$value)
-                                        <option value="{{$value['id']}}" @if(isset($data['employee_id']) && $data['employee_id'] == $value['id']) selected @endif>{{$value['first_name'] ?? ''}} {{$value['last_name'] ?? ''}}</option>
-                                    @endforeach
-                                @endif
-                            </select>
-                        </div>
+                    @php 
+                        $dep_id = $emp_id = '';
+                        if(isset($data['department_id'])){
+                            $dep_id = $data['department_id'];
+                        }
+
+                        if(isset($data['selected_emp'])){
+                            $emp_id = $data['selected_emp'];
+                        }
+                    @endphp
+
+                    {!! App\Helpers\HrmsDepartments("","multiple",$dep_id,"multiple",$emp_id,"") !!}
                         <div class="col-md-3 form-group">
                             <label>From Date</label>
                             <div class="input-daterange input-group" id="date-range">
