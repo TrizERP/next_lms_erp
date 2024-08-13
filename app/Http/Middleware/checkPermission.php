b24f5852b18219f340c21ba88fd7ef65855318ae
robertparker99 <rp2164394@gmail.com>
2024-08-13 14:23:57 +0530

    copied middleware file from live

--- app/Http/Middleware/checkPermission.php
+++ app/Http/Middleware/checkPermission.php
@@ -33,45 +33,47 @@ class checkPermission
             //     $menu_id = $menu->id;
             // }
             // echo "<pre>";print_r($menu_id);exit;
-            $individual = DB::table('tblindividual_rights')->where('menu_id', $menu_id)
-                ->where('profile_id', $userProfileId)
-                ->where('user_id', $user_id)
-                ->where('sub_institute_id', $sub_institute_id)
-                ->first();
+            if($menu_id!=''){
+                $individual = DB::table('tblindividual_rights')->where('menu_id', $menu_id)
+                    ->where('profile_id', $userProfileId)
+                    ->where('user_id', $user_id)
+                    ->where('sub_institute_id', $sub_institute_id)
+                    ->first();
 
-            $group = DB::table('tblgroupwise_rights')->where('menu_id', $menu_id)
-                ->where('profile_id', $userProfileId)
-                ->where('sub_institute_id', $sub_institute_id)
-                ->first();
+                $group = DB::table('tblgroupwise_rights')->where('menu_id', $menu_id)
+                    ->where('profile_id', $userProfileId)
+                    ->where('sub_institute_id', $sub_institute_id)
+                    ->first();
 
-            if (!empty($individual)) {
-                $permissions = $individual;
-            } else {
-                $permissions = $group;
-            }
+                if (!empty($individual)) {
+                    $permissions = $individual;
+                } else {
+                    $permissions = $group;
+                }
 
-            $can_view = $permissions->can_view ?? 0;
-            $can_add = $permissions->can_add ?? 0;
-            $can_edit = $permissions->can_edit ?? 0;
-            $can_delete = $permissions->can_delete ?? 0;
+                $can_view = $permissions->can_view ?? 0;
+                $can_add = $permissions->can_add ?? 0;
+                $can_edit = $permissions->can_edit ?? 0;
+                $can_delete = $permissions->can_delete ?? 0;
 
-            // check methods 
-            
-            // if (empty($permissions)) {
-            //     throw new AuthorizationException('You do not have the necessary permissions to access this resource.');
-            // }
-
-            if ($request->get('_method')=="PUT" && $can_edit != 1) {
-                throw new AuthorizationException('You do not have permission to edit this resource.');
-            } elseif ($request->get('_method')=="POST" && $can_add != 1) {
-                throw new AuthorizationException('You do not have permission to add this resource.');
-            } elseif ($request->get('_method')=="DELETE" && $can_delete != 1) {
-                throw new AuthorizationException('You do not have permission to delete this resource.');
-            } 
-            else {
-                // if ($can_view != 1) {
-                //     throw new AuthorizationException('You do not have permission to view this resource.');
+                // check methods 
+                
+                // if (empty($permissions)) {
+                //     throw new AuthorizationException('You do not have the necessary permissions to access this resource.');
                 // }
+
+                if ($request->get('_method')=="PUT" && $can_edit != 1) {
+                    throw new AuthorizationException('You do not have permission to edit this resource.');
+                } elseif ($request->get('_method')=="POST" && $can_add != 1) {
+                    throw new AuthorizationException('You do not have permission to add this resource.');
+                } elseif ($request->get('_method')=="DELETE" && $can_delete != 1) {
+                    throw new AuthorizationException('You do not have permission to delete this resource.');
+                } 
+                else {
+                    // if ($can_view != 1) {
+                    //     throw new AuthorizationException('You do not have permission to view this resource.');
+                    // }
+                }
             }
         }
 
