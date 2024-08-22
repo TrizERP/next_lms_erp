4d72cc0443b233801328322f2a25ff69e9814108
robertparker99 <rp2164394@gmail.com>
2024-08-22 17:20:50 +0530

    whatsapp send message datatable correction

--- resources/views/whatsapp/whatsapp_send_messages/index.blade.php
+++ resources/views/whatsapp/whatsapp_send_messages/index.blade.php
@@ -54,7 +54,7 @@
                                     <td>{{$data['created_by_name'] ?? '-'}}</td>
                                     <td>{!! $data['message_status'] ?? '-' !!}</td>
                                     <td class="imageTD">{!! $data['message'] ?? '-' !!}</td>
-                                    <td><a href="{{$data['attachment']}}" target="_blank">View</a></td>
+                                    <td>@if($data['attachment']!=null) <a href="{{$data['attachment']}}" target="_blank">View</a>@else - @endif</td>
                                 </tr>
                             @endforeach
                             </tbody>
@@ -71,8 +71,47 @@
 <script src="{{ asset("/plugins/bower_components/datatables/datatables.min.js") }}"></script>
 <script>
     $(document).ready(function () {
-        $('#example').DataTable();
-    });
+            var table = $('#example').DataTable({
+                select: true,
+                lengthMenu: [
+                    [100, 500, 1000, -1],
+                    ['100', '500', '1000', 'Show All']
+                ],
+                dom: 'Bfrtip',
+                buttons: [
+                    {
+                        extend: 'pdfHtml5',
+                        title: 'Student Report',
+                        orientation: 'landscape',
+                        pageSize: 'LEGAL',
+                        pageSize: 'A0',
+                        exportOptions: {
+                            columns: ':visible'
+                        },
+                    },
+                    {extend: 'csv', text: ' CSV', title: 'Student Report'},
+                    {extend: 'excel', text: ' EXCEL', title: 'Student Report'},
+                    {extend: 'print', text: ' PRINT', title: 'Student Report'},
+                    'pageLength'
+                ],
+            });
+            //table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+
+            $('#example thead tr').clone(true).appendTo('#example thead');
+            $('#example thead tr:eq(1) th').each(function (i) {
+                var title = $(this).text();
+                $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+
+                $('input', this).on('keyup change', function () {
+                    if (table.column(i).search() !== this.value) {
+                        table
+                            .column(i)
+                            .search(this.value)
+                            .draw();
+                    }
+                });
+            });
+        });
 
 </script>
 @include('includes.footer')
