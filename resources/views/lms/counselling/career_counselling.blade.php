381b0ec5d71fcd5121a409e36cd0c292fef2db77
Rajesh <rajesh.rafaliya@triz.co.in>
2024-09-23 10:38:05 +0530

    live: 2024-09-24

--- resources/views/lms/counselling/career_counselling.blade.php
+++ resources/views/lms/counselling/career_counselling.blade.php
@@ -12,39 +12,44 @@
             </div>
         </div>
 
-            <div class="card">
-                <div class="row">
-                    <div class="col-lg-12 col-sm-12 col-xs-12">
-                        <div class="col-md-12 form-group">
-                            @php
-                                $chapter_link = "https://main--lms-portal-site.netlify.app";
-
-                                $topic_link = "https://main--lms-portal-site.netlify.app/knowing-yourself";
-
-                                $question_link = "https://main--lms-portal-site.netlify.app/education";
-
-                                $lo_link = "";
-
-                                $content_link = "";
-                                @endphp
-                            <a href="{{$chapter_link}}" target="_blank" class="btn btn-info add-new">Thinking about career plan</a>
-
-                            <a href="{{$topic_link}}" target="_blank" class="btn btn-info add-new">Knowing yourself</a>
-
-                            <a href="{{$question_link}}" target="_blank" class="btn btn-info add-new">Career explore</a>
-
-                            <!--<a href="{{$lo_link}}" target="_blank" class="btn btn-info add-new">Match your profile</a>
-
-                            {{-- Bulk content upload : START --}}
-                            <a href="{{$content_link}}" target="_blank" class="btn btn-info add-new">Bulk Content Upload</a>-->
-                            {{-- Bulk content upload : END --}}
-                        </div>
+        <div class="card">
+            <div class="row">
+                <div class="col-lg-3 col-sm-6 col-xs-12">
+                    <div class="form-group">
+                        @php
+                            $chapter_link = "https://main--lms-portal-site.netlify.app";
+                        @endphp
+                        <a href="{{$chapter_link}}" target="_blank" class="btn btn-info add-new" style="width: 100%">Thinking about career plan</a>
                     </div>
                 </div>
-
-
+                <div class="col-lg-3 col-sm-6 col-xs-12">
+                    <div class="form-group">
+                        @php
+                            $topic_link = "https://main--lms-portal-site.netlify.app/knowing-yourself";
+                        @endphp
+                        <a href="{{$topic_link}}" target="_blank" class="btn btn-info add-new" style="width: 100%">Knowing yourself</a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-xs-12">
+                    <div class="form-group">
+                        @php
+                            $question_link = "https://main--lms-portal-site.netlify.app/education";
+                        @endphp
+                        <a href="{{$question_link}}" target="_blank" class="btn btn-info add-new" style="width: 100%">Career explore</a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-sm-6 col-xs-12">
+                    <div class="form-group">
+                        @php
+                            $lo_link = "https://main--lms-portal-site.netlify.app/match-profile"; 
+                        @endphp
+                        <a href="{{$lo_link}}" target="_blank" class="btn btn-info add-new" style="width: 100%">Match your profile</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+</div>    
 
 @include('includes.footerJs')
 @include('includes.footer')
