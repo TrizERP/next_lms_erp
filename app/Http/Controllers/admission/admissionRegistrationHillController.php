be62cadd9c246cfcd07f24542a35cd1cb76e09c7
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2024-10-09 11:35:53 +0530

    Hills School: New Admission Process - SEND EMAIL ERROR

--- app/Http/Controllers/admission/admissionRegistrationHillController.php
+++ app/Http/Controllers/admission/admissionRegistrationHillController.php
@@ -131,7 +131,7 @@ class admissionRegistrationHillController extends Controller
               $emailRequest = Request::create('/', 'POST', [
                 'type' => 'API',
                 'teacher_id' => $created_by,
-                'sub_institute_id' => session()->get('sub_institute_id'),
+                'sub_institute_id' =>$sub_institute_id,
                 'token' => $_REQUEST['_token'],
                 'all_email' => $data['email'],
                 'syear' => $syear,
@@ -141,7 +141,7 @@ class admissionRegistrationHillController extends Controller
             
             //   echo "<pre>";print_r($emailRequest);
               $sendEmailController = new send_email_parents_controller;
-            //   $sendEmail = $sendEmailController->sendEmail($emailRequest);
+              $sendEmail = $sendEmailController->sendEmail($emailRequest);
             }
         }
         // exit;
