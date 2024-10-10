720a58a53182460a8d64b1954aa58e825e539960
Rajesh <rajesh.rafaliya@triz.co.in>
2024-10-10 10:17:20 +0530

    live: 2024-10-10

--- app/Http/Controllers/result/new_result/studentResultController.php
+++ app/Http/Controllers/result/new_result/studentResultController.php
@@ -2803,6 +2803,7 @@ while ($current_date <= $post_end_date) {
                 ->join('tblstudent as s', 'ap.student_id', '=', 's.id')
                 ->join('tblstudent_enrollment as se', function ($join) {
                     $join->on('s.id', '=', 'se.student_id')
+                    ->on('se.syear', '=', 'ap.syear')
                         ->whereNull('se.end_date');
                 })
                 ->select('s.id', 's.first_name', DB::raw('COUNT(DISTINCT ap.attendance_date) AS present_day'))
@@ -3047,9 +3048,13 @@ while ($current_date <= $post_end_date) {
                                     if(isset($obtained_mark_arr[0]) && ($obtained_mark_arr[0]!='N.A.' || $obtained_mark_arr[0]!='EX')){
                                         $total_marks += $w_m;
                                     }
+                                    $tdVal = number_format($convert_mark, 2);
+                                    if(isset($obtained_mark_arr[0]) && $obtained_mark_arr[0]=="AB"){
+                                        $tdVal = "AB";
+                                    }
                                     /* (Hills) Hide by rajesh std-1 display without convert marks */
                                     //$table .= '<td class="data_center" '. $exam_id . '-'.$val->subject_id.'-'.$standard_id.'>' . number_format((float)$obtained_mark_arr[0], 2) .'</td>';
-                                    $table .= '<td class="data_center" ' . $underline . ' ' . $exam_id . '-'.$val->subject_id.'-'.$standard_id.'>' . number_format($convert_mark, 2) . '</td>';
+                                    $table .= '<td class="data_center else" ' . $underline . ' ' . $exam_id . '-'.$val->subject_id.'-'.$standard_id.'>' . $tdVal . '</td>';
                                 }
                                 
                         // echo $exam_id;echo "<pre>";print_r($obtained_mark_sum);
@@ -3218,7 +3223,7 @@ while ($current_date <= $post_end_date) {
                             $exam_head = $title->ExamTitle;
                             if($exam_head == 'Periodic Test'){
                                 $i = 0;
-                                $pts = ($title->standard_id == 3299) ? 3 : 2;
+                                $pts = ($title->standard_id == 3299 || $title->standard_id == 3965) ? 3 : 2;
                                 $printed_titles = []; // Array to keep track of printed titles
                                 foreach ($exam_name as $key => $value) {
                                     if ($i < $pts && !in_array($value->title, $printed_titles) && $value->ExamTitle == 'Periodic Test') {
@@ -3303,7 +3308,7 @@ while ($current_date <= $post_end_date) {
                                     // get mark for total mark 
                                     $ob_main_mark += ($t_m !== 0) ? (($obtained_mark_sum / $t_m) * $w_m) : 0;
                                     // convert marks if best of 2
-                                    if($standard_id==3299){
+                                    if($standard_id==3299 || $standard_id==3965){
                                         if(count($obtained_mark_arr) > 1){
                                             $convert_mark = max($obtained_mark_arr); // get greatest max
                                         }else{
@@ -3315,7 +3320,8 @@ while ($current_date <= $post_end_date) {
                                     
                                     $pt_per = ($convert_mark !== '0.00' && $w_m != 0) ? round(($convert_mark / $w_m) * 100, 0) : 0;
                             
-                                    $underline = ($pt_per < 33 && $academic_type == "upper" && $standard_id!=3299) ? 'style="text-decoration: underline red 2px;"' : '';
+                                    // $underline = ($pt_per < 33 && $academic_type == "upper" && $standard_id!=3299 && $standard_id!=3965) ? 'style="text-decoration: underline red 2px;"' : '';
+                                    $underline = ($pt_per < 33 && $academic_type == "upper") ? 'style="text-decoration: underline red 2px;"' : '';
                                     if(count($obtained_mark_arr) > 1) {
                                         $total_marks += $w_m;
                                         $table .= '<td class="data_center" ' . $underline . ' ' . $exam_id . '-'.$val->subject_id.'-'.$standard_id.'>' . number_format($convert_mark, 2) . '</td>';
@@ -5176,7 +5182,7 @@ private function buildDisciplineTable($decipline_data)
         $syear = session()->get('syear');
         $sub_institute_id = session()->get('sub_institute_id');
         $user_id = session()->get('user_id');
-
+        // echo "<pre>";print_r($student_array);exit;
         foreach ($student_array as $key => $val) {
             $result_data['student_id'] = $val;
             $result_data['term_id'] = $term_id;
@@ -5187,14 +5193,19 @@ private function buildDisciplineTable($decipline_data)
             $result_data['sub_institute_id'] = $sub_institute_id;
             $result_data['created_by'] = $user_id;
             $result_data['html'] = $request->get('html_' . $val);
-
+            // if($val==236243){
+            //     echo "<pre>";print_r($result_data);
+            // }
+            // exit;
             $data = DB::select("SELECT * FROM result_html WHERE student_id = '" . $val . "' AND term_id = '" . $request->get('term_id') . "'
                     AND grade_id = '" . $request->get('grade_id') . "'  AND standard_id = '" . $request->get('standard_id') . "'
                      AND division_id = '" . $request->get('division_id') . "'  AND syear = '" . $request->get('syear') . "'
                      AND sub_institute_id = '" . session()->get('sub_institute_id') . "'
                     ");
             if (count($data) > 0) {
+                // $html = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">'.htmlentities($request->get('html_' . $val));
                 $html = $request->get('html_' . $val);
+                
                 $finalArray['html'] = $html;
                 $finalArray['updated_by'] = $user_id;
                 $finalArray['updated_on'] = NOW();
@@ -5375,7 +5386,8 @@ private function buildDisciplineTable($decipline_data)
                                         foreach($sub_sub_title as $kk => $vv)
                                         {
                                             if(isset($count[$activity_id][$kk])){
-                                                $table .= '<td style="text-align:center;font-size:medium !important;background:white !important;color:black;width:10%;">&#10004</td>';
+                                                $table .= '<td style="text-align:center;font-size:medium !important;background:white !important;color:black;width:10%;"><img src="https://erp.triz.co.in/images/check-hpc.png" alt="checkImg" style="width:10px;height:10px !important">
+</td>';
                                             }else{
                                                 $table .= '<td style="text-align:center;font-size:medium !important;background:white !important;color:black;width:10%;"></td>';
                                             }
@@ -5408,7 +5420,7 @@ private function buildDisciplineTable($decipline_data)
                                         $table .= '<td style="text-align:center;font-size:medium !important;background:white !important;color:black;width:10%;">';
                                         if(isset($get_result_activity_mark[0]) && $get_result_activity_mark[0]->activity_id==$activity_master_id[$ak])
                                         {
-                                            $table .= '&#10004';
+                                            $table .= '<img src="https://erp.triz.co.in/Images/check-hpc.png" alt="checkImg" style="width:16px !important;height:16px !important">';
                                         }
                                         $table .= '</td>';
                                     
@@ -5451,7 +5463,7 @@ private function buildDisciplineTable($decipline_data)
                 //                     $table .= '<td style="text-align:center;font-size:medium !important;background:white !important;color:black;width:10%;">';
                 //                     if(isset($get_result_activity_mark[0]) && $get_result_activity_mark[0]->activity_id==$activity_master_id[$key2])
                 //                     {
-                //                         $table .= '&#10004';
+                //                         $table .= '<img src="https://erp.triz.co.in/images/check-hpc.png" alt="checkImg" style="width:10px;height:10px !important">   ';
                 //                     }
                 //                     $table .= '</td>';
                                   
@@ -5517,13 +5529,16 @@ private function buildDisciplineTable($decipline_data)
                 }
                 $post_start_date = date('Y-m-d', strtotime($post_start_date . ' +1 day'));
             }
+//echo $attTotDays." - ".count($calArr);
+//exit();            
             $attTotDays = $attTotDays - count($calArr);
 
-            // db::enableQueryLog();
+            //db::enableQueryLog();
             $attarray = DB::table('attendance_student as ap')
             ->join('tblstudent as s', 'ap.student_id', '=', 's.id')
             ->join('tblstudent_enrollment as se', function ($join) {
             $join->on('s.id', '=', 'se.student_id')
+            ->on('se.syear', '=', 'ap.syear')
             ->whereNull('se.end_date');
             })
             ->select('s.id', 's.first_name', DB::raw('COUNT(DISTINCT ap.attendance_date) AS present_day'))
@@ -5535,8 +5550,11 @@ private function buildDisciplineTable($decipline_data)
             ->whereBetween('ap.attendance_date', [$post_start_date_final, $post_end_date_final])
             ->groupBy('s.id')
             ->get();
-            // dd(db::getQueryLog($attarray));
+            //dd(db::getQueryLog($attarray));
             $attarray = $attarray->pluck('present_day', 'id')->all();
+//echo "<pre>";
+//print_r($attarray);
+//            exit();
             if (isset($attarray[$student_id])) {
                 $attendance = $attarray[$student_id] . '/' . $attTotDays;
             } else {
