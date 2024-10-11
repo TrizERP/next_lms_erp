5bfb5d68cabaa0c1d4177916b36760e2a2527b42
Ajit Singh Parihar <ajitsinghprhr123@gmail.com>
2024-10-11 15:21:28 +0530

    CC v2.0

---
+++ resources/views/lms/counselling/career_counselling_ky.blade.php
@@ -0,0 +1,12 @@
+@include('includes.headcss')
+@include('includes.header')
+@include('includes.sideNavigation')
+<div class="container-fluid">
+            <div class="col-lg-12 col-sm-12 col-xs-12">
+                <div class="col-md-12 form-group">
+                    <iframe src="https://careercounseling.vercel.app/knowing-yourself" width="110%" height="600px" style="border:none;"></iframe>
+                </div>
+            </div>
+</div>
+@include('includes.footerJs')
+@include('includes.footer')
