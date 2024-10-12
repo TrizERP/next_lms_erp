ba4f35a0f9fca7314478647e2539b68c5769a429
Ajit Singh Parihar <ajitsinghprhr123@gmail.com>
2024-10-12 11:20:23 +0530

    Respnsiveness Added

--- resources/views/lms/counselling/career_counselling_education.blade.php
+++ resources/views/lms/counselling/career_counselling_education.blade.php
@@ -3,9 +3,9 @@
 @include('includes.sideNavigation')
 <div class="container-fluid">
             <div class="col-lg-12 col-sm-12 col-xs-12">
-                <div class="col-md-12 form-group">
-                    <iframe src="https://careercounseling.vercel.app/education" width="110%" height="600px" style="border:none;"></iframe>
-                </div>
+            <div class="col-md-12 form-group iframe-container">
+            <iframe src="https://careercounseling.vercel.app/education" class="responsive-iframe" style="border:none;"></iframe>
+            </div>
             </div>
 </div>
 @include('includes.footerJs')
