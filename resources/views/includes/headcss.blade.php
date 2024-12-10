17f88771dbbe427e2d6cb5c6cb9a589b61cacb17
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2024-12-10 12:12:33 +0530

    font awsome cdn set for icons

--- resources/views/includes/headcss.blade.php
+++ resources/views/includes/headcss.blade.php
@@ -51,7 +51,8 @@
           type="text/css"/>
     <noscript><link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css"></noscript>
           
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
     <style type="text/css">
         @media print {
             .pagebreak {
