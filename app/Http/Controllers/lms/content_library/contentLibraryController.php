ff8ae30f91e320a33259e984e9e91dd364c3d9de
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2025-01-09 16:08:06 +0530

    content library copy and delete function

--- app/Http/Controllers/lms/content_library/contentLibraryController.php
+++ app/Http/Controllers/lms/content_library/contentLibraryController.php
@@ -43,16 +43,16 @@ class contentLibraryController extends Controller
             $getData = contentLibraryModel::leftJoin('user_activities as ua',function($join) use($user_id){
                 $join->on('ua.content_id','=','contents.id')->where('ua.user_id','=',$user_id)->whereNull('ua.deleted_at');
             })
-            ->selectRaw('contents.*,ua.*,group_concat(DISTINCT ua.action) as all_actions,contents.id as id')
+            ->selectRaw('contents.*,ua.*,group_concat(DISTINCT ua.action) as all_actions,contents.id as id,contents.sub_institute_id as sub_institute_id')
             ->when($request->has('title'),function($q) use ($request){
                 $q->whereRaw('contents.title LIKE "%'.$request->title.'%"');
             })
-            ->whereNull('contents.deleted_at');
-            // ->where('status','approved')
+            ->whereNull('contents.deleted_at')
+            ->where('contents.status','approved');
             foreach ($request->keywords as $key => $value) {
                 if(isset($value)){
                     $searchWord = '"'.$key.'":"'.$value.'"';
-                    $getData->orWhereRaw("contents.keywords LIKE '%".$searchWord."%'");
+                    $getData->WhereRaw("contents.keywords LIKE '%".$searchWord."%'");
                     $res['keywordSearch'][$key] = $value; 
                 }
             }
@@ -62,6 +62,7 @@ class contentLibraryController extends Controller
             $res['searchedContent'] = $getData;
             $res['searchedTitle'] = $request->title;
         }
+
         $starredContent = userActivityModel::Join('contents as c','c.id','=','user_activities.content_id')
         ->selectRaw('c.*,user_activities.*,c.id as id,group_concat(DISTINCT user_activities.action) as all_actions')
         ->where('user_activities.action','starred')
@@ -69,6 +70,7 @@ class contentLibraryController extends Controller
         ->where('user_activities.user_id',$user_id)
         ->whereNull('user_activities.deleted_at')
         ->whereNull('c.deleted_at')
+        ->groupBy('c.id')
         ->get()->toArray();
 
         $downloadContent = userActivityModel::Join('contents as c','c.id','=','user_activities.content_id')
@@ -97,7 +99,6 @@ class contentLibraryController extends Controller
         ->selectRaw('contents.*,ua.*,group_concat(DISTINCT ua.action) as all_actions,contents.id as id')
         ->where('contents.sub_institute_id',$sub_institute_id)
         ->where('contents.created_by',$user_id)
-        // ->where('contents.status',"approved")
         ->whereNull('contents.deleted_at')
         ->groupBy('contents.id')
         ->get()->toArray();
@@ -122,7 +123,7 @@ class contentLibraryController extends Controller
         $getMapVal = $this->getMapVals();
         $res['mapType'] = $getMapVal['mapType'];
         $res['mapValue'] = $getMapVal['mapValue'];
-        // echo "<pre>";print_r($ownContent);exit;
+        // echo "<pre>";print_r($res);exit;
         return is_mobile($type, 'lms/content_library/index', $res, 'view');
     }
 
@@ -217,9 +218,14 @@ class contentLibraryController extends Controller
                 ,'user_id'=>$user_id])->whereNull('deleted_at')->get()->first();
 
                 if(in_array($action,["starred","copy"]) && !empty($checkData)){
-                       $starred=userActivityModel::where('id',$checkData->id)->update(['deleted_at'=>now()]);
-                       $res['status_code'] = 2;
-                       $res['message'] = "Removed Starred Contents !";
+                    if($action=="starred"){
+                        $starred=userActivityModel::where('id',$checkData->id)->update(['deleted_at'=>now()]);
+                        $res['status_code'] = 2;
+                        $res['message'] = "Removed Contents !";
+                    }else{
+                        $res['status_code'] = 2;
+                        $res['message'] = "Content Already Copied !";
+                    }
                 }else{
                     $insert= userActivityModel::insert([
                         'user_id'=>$user_id,
@@ -228,6 +234,23 @@ class contentLibraryController extends Controller
                         'sub_institute_id'=>$sub_institute_id,
                         'created_at'=>now(),
                     ]);
+                        if($action=="copy"){
+                            $contentData=contentLibraryModel::find($content_id);
+                            if(empty($contentData) && $contentData->sub_institute_id!=$sub_institute_id){
+                                $addData = [
+                                    'title'=>$contentData->title,
+                                    'description'=>$contentData->description,
+                                    'keywords'=>$contentData->keywords,
+                                    'attachment'=>$contentData->attachment,
+                                    'status'=>"copied",
+                                    'sub_institute_id'=>$sub_institute_id,
+                                    'parent_id'=>$content_id,
+                                    'created_by'=>$user_id,
+                                    'created_at'=>now(),
+                                ];
+                                $insert2 = contentLibraryModel::insert($addData);
+                            }
+                        }
                     $res['status_code'] = 1;
                     $res['message'] = "Content ".$action." Successfully !";
                 }
@@ -264,6 +287,7 @@ class contentLibraryController extends Controller
         $res['mapType'] = $getMapVal['mapType'];
         $res['mapValue'] = $getMapVal['mapValue'];
         $res['editData'] = contentLibraryModel::find($id);
+        $res['contentData'] = DB::table('content_master')->where(['sub_institute_id'=>$sub_institute_id,'content_library_id'=>$id])->first();
 
         return is_mobile($type, 'lms/content_library/edit', $res, 'view');
     }
@@ -273,7 +297,9 @@ class contentLibraryController extends Controller
         $type = $request->input('type');
         $sub_institute_id = session()->get('sub_institute_id');
         $user_id = session()->get('user_id');
-        
+        $syear = session()->get('syear');
+
+        // echo "<pre>";print_r($request->all());exit;
         if($type=="API"){
             try {
                 if (!$this->jwtToken()->validate()) {
@@ -287,7 +313,8 @@ class contentLibraryController extends Controller
                 return response()->json($response, 401);
             }
             $sub_institute_id = $request->get('sub_institute_id');
-            $user_id = $request->get('user_id');            
+            $user_id = $request->get('user_id');         
+            $syear = $request->get('syear');            
         }
         $res['status_code'] = 0;
         $res['message'] = "Failed to Update";
@@ -312,9 +339,44 @@ class contentLibraryController extends Controller
         ];
         
         $update = DB::table('contents')->where('id',$id)->update($updateDate);
+
         if($update){
             $res['status_code'] = 1;
             $res['message'] = "Content Updated Successfully !";
+        }
+
+        if($request->has('grade') && $request->has('standard') && $request->has('subject') && $request->has('chapter_id')){
+            $checkMaster = DB::table('content_master')->where(['sub_institute_id'=>$sub_institute_id,'content_library_id'=>$id])->first();
+            $masterData = [
+                'grade_id'=>$request->grade,
+                'standard_id'=>$request->standard,
+                'subject_id'=>$request->subject,
+                'chapter_id'=>$request->chapter_id,
+                'topic_id'=> isset($request->topic_id) ? $request->topic_id : null,
+                'title'=>$request->title,
+                'description'=>$request->description,
+                'filename'=>"https://s3-triz.fra1.cdn.digitaloceanspaces.com/public/content_library/".$filename,
+                'file_type'=>'link',
+                'url'=>"https://s3-triz.fra1.cdn.digitaloceanspaces.com/public/content_library/".$filename,
+                'show_hide'=> isset($request->show_hide) ? 1 : 0,
+                'content_category'=>'Content Library',
+                'syear'=>$syear,
+                'sub_institute_id'=>$sub_institute_id,
+                'content_library_id'=>$id
+            ];
+
+            if(empty($checkMaster)){
+                $masterData['created_at']=now();
+                $masterData['created_by']=$user_id;
+                $actionStatus = DB::table('content_master')->insert($masterData);
+            }else{
+                $actionStatus = DB::table('content_master')->where(['sub_institute_id'=>$sub_institute_id,'content_library_id'=>$id])->update($masterData);
+            }
+
+            if(!$actionStatus){
+                $res['status_code'] = 0;
+                $res['message'] = "Content Master Failed to Add!";
+            }
         }
 
         return is_mobile($type, 'content_library.index', $res);
@@ -344,7 +406,14 @@ class contentLibraryController extends Controller
         $res['status_code'] = 0;
         $res['message'] = "Failed to Delete";
 
-        $delete =DB::table('contents')->where('id',$id)->update(['deleted_at'=>now()]);
+        $contentData = contentLibraryModel::find($id);
+
+        if($contentData){
+           $userActivity = DB::table('user_activities')->where(['sub_institute_id'=>$sub_institute_id,'user_id'=>$user_id,'action'=>'copy'])->where('content_id',$contentData->parent_id)->update(['deleted_at'=>now()]);
+        }
+        
+        $delete = DB::table('contents')->where('id',$id)->update(['deleted_at'=>now()]);
+        
         if($delete){
             $res['status_code'] = 1;
             $res['message'] = "Content Deleted Succefully";
