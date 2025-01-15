f46c012ea16defe1d119d93fbaa77906eba5270a
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2025-01-15 16:40:07 +0530

    library reports excel buttons

--- resources/views/library/library_report.blade.php
+++ resources/views/library/library_report.blade.php
@@ -233,6 +233,47 @@
             }
         });
     }
+    
+    $(document).ready(function () {
+        var table = $('#example').DataTable({
+            select: true,
+            lengthMenu: [
+                [100, 500, 1000, -1],
+                ['100', '500', '1000', 'Show All']
+            ],
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'pdfHtml5',
+                    title: 'Fees Monthly Report',
+                    orientation: 'landscape',
+                    pageSize: 'LEGAL',
+                    pageSize: 'A0',
+                    exportOptions: {
+                        columns: ':visible'
+                    },
+                },
+                {extend: 'csv', text: ' CSV', title: 'Fees Monthly Report'},
+                {extend: 'excel', text: ' EXCEL', title: 'Fees Monthly Report'},
+                {extend: 'print', text: ' PRINT', title: 'Fees Monthly Report'},
+                'pageLength'
+            ],
+        });
 
+        $('#example thead tr').clone(true).appendTo('#example thead');
+        $('#example thead tr:eq(1) th').each(function (i) {
+            var title = $(this).text();
+            $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+
+            $('input', this).on('keyup change', function () {
+                if (table.column(i).search() !== this.value) {
+                    table
+                        .column(i)
+                        .search( this.value )
+                        .draw();
+                }
+            } );
+        } );
+    } );
 </script>
 @include('includes.footer')
