18dd7a13980862a1659050a79f2f46283d0bb575
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2025-01-24 14:51:22 +0530

    student monthly attendance report json api

--- app/Http/Controllers/student/studentAttendanceController.php
+++ app/Http/Controllers/student/studentAttendanceController.php
@@ -454,7 +454,7 @@ class studentAttendanceController extends Controller
         $sub_institute_id = session()->get('sub_institute_id');
         $batch="";
 
-        if($type=="API"){
+        if(in_array($type,["API","JSON"])){
             $syear = $request->get('syear');
             $sub_institute_id = $request->get('sub_institute_id');
         }
