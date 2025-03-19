a78ab42b8d47564ae8a73188d31907665047d244
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2025-03-19 18:05:08 +0530

    student family history

--- app/Http/Controllers/student/tblstudentFamilyHistoryController.php
+++ app/Http/Controllers/student/tblstudentFamilyHistoryController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use function App\Helpers\is_mobile;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Session;
+use Carbon\Carbon;
 
 class tblstudentFamilyHistoryController extends Controller
 {
@@ -37,7 +40,140 @@ class tblstudentFamilyHistoryController extends Controller
      * @param  Request  $request
      * @return Response
      */
-    public function store(Request $request)
+    public function store(Request $request){
+        // echo "<pre>";print_r($request->all());exit;
+        $type = $request->input('type');
+        $sub_institute_id = $request->session()->get('sub_institute_id');
+        $syear = $request->session()->get('syear');
+        $user_id = $request->session()->get('user_id');
+        $insertNames = $request->insert['name'] ?? [];
+        $updateNames = $request->update['name'] ?? [];
+
+        if(in_array($type,['API','JSON'])){
+
+            $sub_institute_id = $request->input('sub_institute_id');
+            $syear = $request->input('syear');
+            $user_id = $request->input('user_id');
+
+            $validator = Validator::make($request->all(), [
+                'sub_institute_id'  => 'required|numeric',
+                'syear'   => 'required|numeric',
+                'user_id'   => 'required|numeric',
+            ]);
+            
+            if ($validator->fails()) {
+                $res['status'] = '0';
+                $res['response'] = $validator->messages()->first();
+		        return is_mobile($type, "student/edit_student", $res, "view");
+            } 
+        }
+        $i = 0;
+        // insert into table
+        if(!empty($insertNames)){
+            foreach ($insertNames as $key => $name) {
+                if($name!=''){
+                    $insertrelation = isset($request->insert['relation_with_students'][$key]) ? $request->insert['relation_with_students'][$key] : null;
+                    $insertDob = isset($request->insert['dob'][$key]) ? $request->insert['dob'][$key] : null;
+                    $insertDob = $insertDob ? Carbon::createFromFormat('d-m-Y', $insertDob)->format('Y-m-d') : null;
+                    $insertBloodgroup = isset($request->insert['bloodgroup'][$key]) ? $request->insert['bloodgroup'][$key] : null;
+                    $insertInstitute = isset($request->insert['institute_name'][$key]) ? $request->insert['institute_name'][$key] : null;
+                    $insertCourse = isset($request->insert['courses'][$key]) ? $request->insert['courses'][$key] : null;
+                    $insertPercentage = isset($request->insert['percentage'][$key]) ? $request->insert['percentage'][$key] : null;
+                    $insertDesignation = isset($request->insert['designation'][$key]) ? $request->insert['designation'][$key] : null;
+                    $insertCompany = isset($request->insert['company_name'][$key]) ? $request->insert['company_name'][$key] : null;
+                    $insertOffice = isset($request->insert['office_address'][$key]) ? $request->insert['office_address'][$key] : null;
+                    $insertIncome = isset($request->insert['annual_income'][$key]) ? $request->insert['annual_income'][$key] : null;
+                    $insertMobile = isset($request->insert['mobile_no'][$key]) ? $request->insert['mobile_no'][$key] : null;
+                    $insertGuardian = isset($request->insert['local_guardian'][$key]) ? $request->insert['local_guardian'][$key] : null;
+                    $insertYear = isset($request->insert['year']) ? $request->insert['year'] : null;
+                    $i++;
+                    tblstudentFamilyHistoryModel::insert([
+                        'student_id'=>$request->student_id,
+                        'name'=>$name,
+                        'institute_name'=>$insertInstitute,
+                        'course'=>$insertCourse,
+                        'year'=>$insertYear,
+                        'percentage'=>$insertPercentage,
+                        'relation_with_student'=>$insertrelation,
+                        'annual_income'=>$insertIncome,
+                        'birthdate'=>$insertDob,
+                        'blood_group'=>$insertBloodgroup,
+                        'designation'=>$insertDesignation, 
+                        'company_name'=>$insertCompany,
+                        'office_address'=>$insertOffice, 
+                        'mobile_no'=>$insertMobile,
+                        'guardian_address'=>$insertGuardian, 
+                        'sub_institute_id'=>$sub_institute_id, 
+                        'created_by'=>$user_id, 
+                        'created_on'=>now(), 
+                    ]);
+        // echo "<pre>";print_r($insertDob);
+
+                }
+            }
+        }
+        // exit;
+        // update into table
+        if(!empty($updateNames)){
+            foreach ($updateNames as $id => $name) {
+                if($name!=''){
+                    $updaterelation = isset($request->update['relation_with_students'][$id]) ? $request->update['relation_with_students'][$id] : null;
+                    $updateDob = isset($request->update['dob'][$id]) ? $request->update['dob'][$id] : null;
+                    $updateDob = $updateDob ? Carbon::createFromFormat('d-m-Y', $updateDob)->format('Y-m-d') : null;
+                    $updateBloodgroup = isset($request->update['bloodgroup'][$id]) ? $request->update['bloodgroup'][$id] : null;
+                    $updateInstitute = isset($request->update['institute_name'][$id]) ? $request->update['institute_name'][$id] : null;
+                    $updateCourse = isset($request->update['courses'][$id]) ? $request->update['courses'][$id] : null;
+                    $updateYear = isset($request->update['year'][$id]) ? $request->update['year'][$id] : null;
+                    $updatePercentage = isset($request->update['percentage'][$id]) ? $request->update['percentage'][$id] : null;
+                    $updateDesignation = isset($request->update['designation'][$id]) ? $request->update['designation'][$id] : null;
+                    $updateCompany = isset($request->update['company_name'][$id]) ? $request->update['company_name'][$id] : null;
+                    $updateOffice = isset($request->update['office_address'][$id]) ? $request->update['office_address'][$id] : null;
+                    $updateIncome = isset($request->update['annual_income'][$id]) ? $request->update['annual_income'][$id] : null;
+                    $updateMobile = isset($request->update['mobile_no'][$id]) ? $request->update['mobile_no'][$id] : null;
+                    $updateGuardian = isset($request->update['local_guardian'][$id]) ? $request->update['local_guardian'][$id] : null;
+                    $i++;
+                    tblstudentFamilyHistoryModel::where('id',$id)->update([
+                        'name'=>$name,
+                        'institute_name'=>$updateInstitute,
+                        'course'=>$updateCourse,
+                        'year'=>$updateYear,
+                        'percentage'=>$updatePercentage,
+                        'relation_with_student'=>$updaterelation,
+                        'annual_income'=>$updateIncome,
+                        'birthdate'=>$updateDob,
+                        'blood_group'=>$updateBloodgroup,
+                        'designation'=>$updateDesignation, 
+                        'company_name'=>$updateCompany,
+                        'office_address'=>$updateOffice, 
+                        'mobile_no'=>$updateMobile,
+                        'guardian_address'=>$updateGuardian,
+                        'created_by'=>$user_id, 
+                        'updated_on'=>now(), 
+                    ]);
+                }
+            }
+        }
+
+        if($i==0){
+            $res['status'] = 0;
+            $res['message'] = 'Failed to add data!!';
+            Session::flash('status', 0); 
+            Session::flash('message', 'Failed to add data!!'); 
+        }
+        else{
+            $res['status'] = 1;
+            $res['message'] = 'Data added successfully!!';
+            Session::flash('status', 1); 
+            Session::flash('message', 'Data added successfully!!'); 
+        }
+
+        if(in_array($type,['API','JSON'])){
+            return response()->json($res);
+        }
+        return redirect()->back();
+    }
+    // changed function name on 19-03-2025 for adding new fields 
+    public function storeOld(Request $request)
     {
         $sub_institute_id = $request->session()->get('sub_institute_id');
         $term_id = $request->session()->get('term_id');
---
+++ database/migrations/2025_03_19_130411_add_columns_student_family_past.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // add columns in table tblstudent_past_education
+        Schema::table('tblstudent_past_education', function (Blueprint $table) {
+            $table->integer('reason_of_leaving')->after('trial')->nullable();
+            $table->biginteger('created_by')->after('sub_institute_id')->nullable();
+            $table->timestamp('updated_on')->after('created_on')->nullable();
+        });
+
+         // add columns in table tblstudent_past_education
+        Schema::table('tblstudent_family_history', function (Blueprint $table) {
+            $table->date('birthdate')->after('annual_income')->nullable();
+            $table->string('blood_group',255)->after('birthdate')->nullable();
+            $table->string('designation',255)->after('blood_group')->nullable();
+            $table->string('company_name',255)->after('designation')->nullable();
+            $table->string('office_address',255)->after('company_name')->nullable();
+            $table->string('mobile_no',255)->after('office_address')->nullable();
+            $table->mediumText('guardian_address')->after('mobile_no')->nullable();
+            $table->biginteger('created_by')->after('sub_institute_id')->nullable();
+            $table->timestamp('updated_on')->after('created_on')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // add columns in table tblstudent_past_education
+        Schema::table('tblstudent_past_education', function (Blueprint $table) {
+            $table->dropColumn('reason_of_leaving');
+            $table->dropColumn('created_by');
+            $table->dropColumn('updated_on');
+        });
+
+         // add columns in table tblstudent_past_education
+        Schema::table('tblstudent_family_history', function (Blueprint $table) {
+            $table->dropColumn('birthdate');
+            $table->dropColumn('blood_group');
+            $table->dropColumn('designation');
+            $table->dropColumn('company_name');
+            $table->dropColumn('office_address');
+            $table->dropColumn('mobile_no');
+            $table->dropColumn('guardian_address');
+            $table->dropColumn('created_by');
+            $table->dropColumn('updated_on');
+        });
+    }
+};
