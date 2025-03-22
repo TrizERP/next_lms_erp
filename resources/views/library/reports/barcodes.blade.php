0e7df8105fa5c49d56dbdd29e3db690ac20e414e
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2025-03-22 12:46:05 +0530

    mmis library print barcode size changes

--- resources/views/library/reports/barcodes.blade.php
+++ resources/views/library/reports/barcodes.blade.php
@@ -12,12 +12,22 @@
 </head>
 <body>
 <table style="width:100%;border-collapse: collapse;">
-    @for ($i = 0; $i < count($barcodes); $i+=3)
+    @php 
+        $columns = 3;
+        $padding = 16;
+        $fontSize = '0.8rem';
+        if($print_type=="member"){
+            $columns = 5;
+            $padding = 2;
+            $fontSize = '0.6rem';
+        }
+    @endphp
+    @for ($i = 0; $i < count($barcodes); $i+=$columns)
         <tr>
-            @for ($j = $i; $j < $i + 3 && $j < count($barcodes); $j++)
-                <td style="text-align: center; padding: 16px;">
+            @for ($j = $i; $j < $i + $columns && $j < count($barcodes); $j++)
+                <td style="text-align: center; padding: {{$padding}}px;">
                     <img class="barcode-image" src="data:image/png;base64,{{ base64_encode($barcodes[$j]['image']) }}" alt="{{ $barcodes[$j]['code'] }}">   
-                <p style="font-size: 0.6rem;text-align:center;">@if(isset($barcodes[$j]['other']) && $barcodes[$j]['other']=="member" ) {{ substr($barcodes[$j]['title'],0,25) }} @else {{ substr($barcodes[$j]['title'],0,20) }} @if($barcodes[$j]['other']!='') - {{$barcodes[$j]['other']}} @endif @endif</p>  
+                <p style="font-size:{{$fontSize}};text-align:center;margin:0px;"><b>@if(isset($barcodes[$j]['other']) && $barcodes[$j]['other']=="member" ) {{ substr($barcodes[$j]['title'],0,25) }} @else {{ substr($barcodes[$j]['title'],0,20) }} @if($barcodes[$j]['other']!='') - {{$barcodes[$j]['other']}} @endif @endif<b></p>  
                 </td>
             @endfor
         </tr>
