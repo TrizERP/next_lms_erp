fa480ab6270b49eaa8a853be9f7c1a74abb7573e
Rajesh Triz <rajesh.rafaliya@triz.co.in>
2025-04-01 15:59:57 +0530

    development:01-04-2025 by uma

--- routes/skill.php
+++ routes/skill.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\skill\SkillMatrixController;
+use App\Http\Controllers\lms\library\jobOccupationController;
+use App\Http\Controllers\lms\library\jobroleSkillController;
+use App\Http\Controllers\lms\library\jobroleTaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'skill', 'middleware' => ['session', 'menu', 'logRoute','check_permissions']], function() {
@@ -10,4 +13,7 @@ Route::group(['prefix' => 'skill', 'middleware' => ['session', 'menu', 'logRoute
     Route::get('/jobdescription', [SkillMatrixController::class, 'JobDescription'])->name('jobrole.jobdescription');
     Route::get('/assessment_library', [SkillMatrixController::class, 'AssessmentLibrary'])->name('assessment_library');
     Route::view('/gap_analysis', 'skill.assessment.gap_analysis')->name('gap_analysis');
+    Route::resource('jobroleOccupation',jobOccupationController::class); // added on 01-04-2025
+    Route::resource('jobroleSkill',jobroleSkillController::class); // added on 01-04-2025
+    Route::resource('jobroleTask',jobroleTaskController::class); // added on 01-04-2025
 });
