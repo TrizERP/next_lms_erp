5e0c44efded14570779aca5991715bb00f2c1bff
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2025-04-01 12:02:45 +0530

    hp api jobrole

---
+++ app/Models/lms/library/jobroleSkillModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\lms\library;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class jobroleSkillModel extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/lms/library/jobroleTaskModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\lms\library;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class jobroleTaskModel extends Model
+{
+    use HasFactory;
+}
