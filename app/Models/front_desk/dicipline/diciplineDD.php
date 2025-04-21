9921ca9237696112d2f51c848cc628bd8f3e9c2d
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2025-04-21 17:20:21 +0530

    Merge pull request #425 from TrizERP/rp_dev
    Rp dev TT41

---
+++ app/Models/front_desk/dicipline/diciplineDD.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\front_desk\dicipline;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class diciplineDD extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'dicipline_dd';
+}
