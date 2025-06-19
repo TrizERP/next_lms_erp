db0588afc2fdb71f7e2771bad27f5aea49fac4dc
Rajesh <rajesh.rafaliya@triz.co.in>
2025-06-19 10:47:56 +0530

    live:19-06-2025 by uma

--- resources/views/user/edit_user.blade.php
+++ resources/views/user/edit_user.blade.php
@@ -94,12 +94,12 @@ br {
                     @endphp
                     <!-- tabs starts  -->
                     <div class="tab-content">
-                        @if(session('success'))
+                        <!-- @if(session('success'))
                             <div class="alert alert-success alert-block">
                                 <button type="button" class="close" data-dismiss="alert">×</button>
                                 <strong>{{ session('success') }}</strong>
                             </div>
-                        @endif
+                        @endif -->
                     <!-- tab 1 start  -->
                     <div class="tab-pane p-3 active" id="section-linemove-1" role="tabpanel">     
                     <form action="{{ route('add_user.update', $data['id']) }}" enctype="multipart/form-data" method="post">
