2ddf3d68536f2e6834c94047e02310a40262da18
Rajesh Triz <rajesh.rafaliya@triz.co.in>
2025-08-23 13:19:05 +0530

    hills email template add

---
+++ resources/views/admission/registrationHills/sendConfirmEmail.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Admission confirmation</title>
+</head>
+<style>
+    body {
+        padding: 10px;
+    }
+
+</style>
+
+<body>
+
+    <div style="border:3px solid black;padding:1px;">
+        <div style="border:1px solid black;padding:6px;">
+            <div class="logo-section" style="text-align: right;">
+                <img src="https://erp.triz.co.in/admin_dep/images/hills_logo1.png" width="150" height="50" />
+            </div>
+            <div class="content-div" style="color:black;">
+                <h2 class="h1line underline" style="text-align: center;text-decoration: underline;">{{$admission_std}} ADMISSION PROCEDURE AT HILLS HIGH SCHOOL, A.Y- {{$aca_year}}</h2>
+                <p style="font-size:16px;">Dear Parents,</p>
+                <p style="text-decoration:underline;font-size:16px;">Dear Parents,Please note that the issue of Admission Forms DOES NOT GUARANTEE
+                    ADMISSION.</p>
+
+                <h3 style="text-decoration:underline;">CONTACT WITH HILLS’ HIGH SCHOOL</h3>
+                <ul>
+                    <li style="font-size:16px">Please note that all communication with the Parents will be done via e- mail ONLY.
+                        The school mail id is admission@hillshigh.com / session2@hillshigh.com .</li>
+                    <li style="font-size:16px">The STATUS of admission of your child’s admission will be communicated via email.</li>
+                </ul>
+
+                <h3 style="text-decoration:underline;">ADMISSION PROCEDURE</h3>
+                <p style="font-size:16px;">The Procedure for admissions at Hills High School has 4 stages</p>
+
+                <h3 style="text-decoration:underline;">STAGE-1 COLLECTION OF FORM</h3>
+                <ul>
+                    <li style="font-size:16px">Collection of Admission Form from Hills’ High School, Vesu at the date & time given.</li>
+                </ul>
+
+                <h3 style="text-decoration:underline;">STAGE 2 Activity session :</h3>
+                <ul>
+                    <li style="font-size:16px">A fun filled activity session will be conducted at Hills’ High School, Vesu, on <b>{{$parent_date}} at
+                       {{$parent_time}}</b></li>
+                    <li style="font-size:16px">Child should be present with <span style="background-color: yellow;">current School I-card.</span></li>
+                    <li style="font-size:16px">The session would be for approx 45mins.</li>
+                </ul>
+
+                <h3 style="text-decoration:underline;">STAGE 3- Parents Interaction (If the students Activity session is cleared): </h3>
+                <ul>
+                    <li style="font-size:16px">Parents interaction with the Admission committee will be held at a specific date and time. For
+                        the interaction, it is COMPULSORY FOR BOTH MOTHER & FATHER TO BE PRESENT. Please do not bring
+                        the child for the parents interaction.</li>
+                </ul>
+
+                <h3 style="text-decoration:underline;">STAGE 4- Submission of form and required documents:</h3>
+                <p style="text-indent: 3ch;font-size:16px;">Candidates who are selected after the interaction will need to submit all
+                    the necessary documents along with the admission form & fees to Hills’ High School office, within
+                    the stipulated time, to complete the Provisional Admission procedure.</p>
+                <ul style="margin:0px 8px;">
+                    <li style="font-size:16px"><b>PLEASE NOTE :-</b> In case parents or child are unable to attend any of the sessions on the
+                        dates / time allotted by us, kindly send a mail to admission@hillshigh.com /
+                        session2@hillshigh.com so that we can arrange an alternative date / time. In case parents or
+                        child are unable to attend any of the sessions on the dates / time allotted by us, kindly send a
+                        mail to <a href="mailto:admission@hillshigh.com">admission@hillshigh.com</a> / <a href="mailto:session2@hillshigh.com">session2@hillshigh.com</a> so that we can arrange an alternative
+                        date / time.</li>
+                </ul>
+                <p style="font-size:16px;"><b>Selection of Shift – </b>Morning or Noon will be on the basis of first come first serve, Once the
+                    total number of students are enrolled in either morning or afternoon session admission will be
+                    granted only in the other session</p>
+
+                <div class="thank-you" style="text-align:center">
+                    <h3 style="font-family: Arial Narrow;font-size:16px;">Wishing you all the Best!!!</h3>
+                    <h3 style="font-family: Lucida Handwriting;">THANK YOU!! <br>Hills High Family</h3>
+                    <p style="font-size:16px;"><b style="font-family: Arial Narrow;">Address:</b> 65-A RCC Canal Road Opp.Nandavan Apt,Vesu, Surat, Gujarat 395007.<br>
+                        Contact Number-9033095477 / 3477 <span style="color:red;">Connect us on Instagram</span> :- @hillshighschool <br>
+                        School website – www.hillshigh.com<br> 
+                        Email Id: <a href="mailto:admission@hillshigh.com">admission@hillshigh.com</a> / <a href="mailto:session2@hillshigh.com">session2@hillshigh.com</a> (For any admission query)
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
