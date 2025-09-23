2adc761ed327010541f3fdbb21961b03a3a19310
Rajesh Triz <rajesh.rafaliya@triz.co.in>
2025-09-23 12:03:44 +0530

    Hills admission Confirmation

--- app/Http/Controllers/admission/admissionRegistrationHillController.php
+++ app/Http/Controllers/admission/admissionRegistrationHillController.php
@@ -57,7 +57,7 @@ class admissionRegistrationHillController extends Controller
 
        $res['hnArr'] = [1,2,3];
         $res['pIntArr'] = ["I","NO","W/L"];
-        $res['confArr'] = ["C"]; // ,"C/A","NO","W/L"
+        $res['confArr'] = ["C","C/A","NO","W/L"]; // ,"C/A","NO","W/L"
        $res['yesNo'] = ["Yes","No"];
 
         $res['status_code'] = 1;
@@ -157,11 +157,42 @@ class admissionRegistrationHillController extends Controller
               $sendSmsController = new send_sms_parents_controller;
               $sendSms = $sendSmsController->sendSMS($data['mobile'], $text, $sub_institute_id);
               //send email;
-               if(isset($data["pint_time"]) && isset($pindate) && isset($data['admission_standard']) && in_array($data["pint"],["I"])){
+              if(in_array($data['conf'],["C","C/A"]) && isset($condate) && isset($data['admission_standard'])){
                     $nextYear = ((int) substr($syear, 2, 2)+1);
                     $getStandard = DB::table('standard')->where(['id'=>$data['admission_standard'],'sub_institute_id'=>$sub_institute_id])->first();
 
                     $htmlContent = view('admission.registrationHills.sendConfirmEmail', [
+                        'page_type'=>'confirm',
+                        'conf_date' => $condate,
+                        'conf' => $data["conf"] ?? '',
+                        'parent_time' => '9:00 a.m. to 11:00 am OR 2:30 p.m.  to 4:30 p.m.',
+                        'aca_year'    => $syear.'-'.$nextYear,
+                        'admission_std'    => $getStandard->name ?? '-',
+                    ])->render();
+
+                    // return $htmlContent;exit;
+                    $emailRequest = new Request([
+                        'type' => 'webForm',
+                        'teacher_id' => $created_by,
+                        'sub_institute_id' =>$sub_institute_id,
+                        'token' => $_REQUEST['_token'],
+                        'all_email' => $data['email'],
+                        'subject' => 'ADMISSION PROCEDURE',
+                        'syear' => $syear,
+                        'example_subject' => 'ADMISSION PROCEDURE',
+                        'content' => $htmlContent
+                    ]);
+                    $sendEmailController = new send_email_parents_controller;
+                    $sendEmail = $this->sendEmail($emailRequest);
+            //   echo "<pre>";print_r($sendEmail);exit;
+                    
+              }
+               elseif(isset($data["pint_time"]) && isset($pindate) && isset($data['admission_standard']) && in_array($data["pint"],["I"])){
+                    $nextYear = ((int) substr($syear, 2, 2)+1);
+                    $getStandard = DB::table('standard')->where(['id'=>$data['admission_standard'],'sub_institute_id'=>$sub_institute_id])->first();
+
+                    $htmlContent = view('admission.registrationHills.sendConfirmEmail', [
+                        'page_type'=>'parent',
                         'parent_date' => $pindate,
                         'pint' => $data["pint"] ?? '',
                         'parent_time' => $data["pint_time"] ?? '',
@@ -189,6 +220,7 @@ class admissionRegistrationHillController extends Controller
                     $getStandard = DB::table('standard')->where(['id'=>$data['admission_standard'],'sub_institute_id'=>$sub_institute_id])->first();
 
                     $htmlContent = view('admission.registrationHills.sendConfirmEmail', [
+                        'page_type'=>'parent',
                         'parent_date' => $pindate,
                         'pint' => $data["pint"] ?? '',
                         'parent_time' => $data["pint_time"] ?? '',
@@ -304,7 +336,7 @@ class admissionRegistrationHillController extends Controller
         // echo "<pre>";print_r($request->all());exit;
         $res['hnArr'] = [1,2,3];
         $res['pIntArr'] = ["I","NO","W/L"];
-        $res['confArr'] = ["C"]; // ,"C/A","NO","W/L"
+        $res['confArr'] = ["C","C/A","NO","W/L"]; // ,"C/A","NO","W/L"
         $res['yesNo'] = ["Yes","No"];
         $res['data'] = $data;
         
