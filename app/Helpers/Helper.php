e6a627287e7241f950063dbe902abcb49aac3d6e
Rajesh Rafaliya <rajeshrafaliya@gmail.com>
2025-09-25 14:21:14 +0530

    Admission Standard

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1533,7 +1533,7 @@ if (!function_exists('getStudents')) {
             ->selectRaw("tc.*,s.*,se.syear,se.student_id,se.grade_id,se.standard_id,se.section_id,se.student_quota,se.roll_no,
                 se.start_date,se.end_date,se.enrollment_code,se.drop_code,se.drop_remarks,se.drop_remarks,se.term_id,
                 se.remarks,se.admission_fees,se.house_id,se.lc_number,st.name standard_name,st.short_name as short_standard_name,st.school_stream,s.city,se.standard_id,se.section_id,se.roll_no,
-                se.grade_id,d.name as division_name,s.father_name,s.mother_name,ss.SchoolName as school_name,ss.Mobile as school_mobile,
+                se.grade_id,d.name as division_name,s.father_name,s.mother_name,ss.SchoolName as school_name,ss.Mobile as school_mobile,s.admission_standard,
                 ss.Logo as school_image,ss.ReceiptAddress as school_address,(CASE WHEN s.gender = 'M' then 'male' else 'female' end) as gender,
                 r.religion_name,c.caste_name,s.subcast,s.affiliation_no,s.school_code,s.admission_date,td.first_name AS driver_name,
                 td.mobile AS driver_mobile,td.icard_icon,s.mother_mobile,CONCAT_WS(' ',s.first_name,CONCAT(SUBSTRING(s.father_name,1,1),'.'),
@@ -1633,6 +1633,7 @@ if (!function_exists('getStudents')) {
             $student_data[$value->id]['mother_mobile'] = $value->mother_mobile;
             $student_data[$value->id]['short_student_name'] = $value->short_student_name;
             $student_data[$value->id]['admission_no'] = $value->enquiry_no;
+            $student_data[$value->id]['admission_standard'] = $value->admission_standard;
         }
 
         return $student_data;
