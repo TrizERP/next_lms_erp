cc9358837e3968af8f74df785e779ea912124860
Vivek <vivekgajera2709@gmail.com>
2025-09-25 14:32:26 +0530

    Show all family Member in Student Report

--- app/Http/Controllers/student/studentReportController.php
+++ app/Http/Controllers/student/studentReportController.php
@@ -229,7 +229,7 @@ class studentReportController extends Controller
             ->when(in_array($sub_institute_id,[201,202,203]) && $request->order_by=="enrollment_no", function ($q) {
                 $q->orderByRaw("CAST(SUBSTRING_INDEX(enrollment_no, '/', -1) AS UNSIGNED) ASC");
             })
-            ->groupBy('tblstudent.id')
+            //->groupBy('tblstudent.id')
             ->get();
             $student_dataArr = [];
             if($sub_institute_id==254){
