9daf71c0d11749fbd0d549ff4034a6cb2d01d847
Vivek Gajera <vivekgajera2709@gmail.com>
2025-11-18 11:13:01 +0530

    Merge pull request #503 from TrizERP/vivek_dev
    MMIS Book verification Report add Remarks

--- resources/views/library/bookVarification/varifedReports.blade.php
+++ resources/views/library/bookVarification/varifedReports.blade.php
@@ -24,11 +24,10 @@
             <div>
                 <form action="{{route('verified_report.index')}}" class="row">
                     @csrf
-                    <div class="col-md-4 form-group">
+                    <div class="col-md-6 form-group">
                         <label for="item_code"><b>Item Code</b></label>
-                        <input type="text" class="form-control" name="item_code" id="item_code" @if(isset($data['searchedItem'])) value="{{$data['searchedItem']}}" @else placeholder="Search Item Status Name" @endif ">
+                        <input type="text" class="form-control" name="item_code" id="item_code" @if(isset($data['searchedItem'])) value="{{$data['searchedItem']}}" @else placeholder="Search Item Status Name" @endif style="width:500px;">
                     </div>
-                    <!--
                     <div class="col-md-4 form-group">
                         <label for="Academic Year"><b>Academic Year</b></label>
                         <select name="year" id="year" class="form-control">
@@ -38,11 +37,9 @@
                             @endforeach
                         </select>
                     </div>
-                -->
                     <div class="col-md-2">
                         <input type="submit" value="Search" name="submit" class="btn btn-primary" style="margin-top:22px">
                     </div>
-                    <div class="col-md-6 form-group"></div>
                 </form>
             </div>
 
@@ -55,8 +52,8 @@
                                 <th>Syear</th>
                                 <th>Item Code</th>
                                 <th>Title</th>
-                                <th>Collection Type</th>
-                                <th class="text-left">Remarks</th>
+                                <th>Remarks</th>
+                                <th class="text-left">Collection Type</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -66,8 +63,8 @@
                                 <td>{{$value['syear']}}</td>
                                 <td>{{$value['item_code']}}</td>
                                 <td>{{$value['book_title']}}</td>
+                                <td>{{$value['remarks'] }}</td>
                                 <td>{{$value['collection_type']}}</td>
-                                <td>{{$value['remarks']}}</td>
                             </tr>
                             @endforeach
                         </tbody>
