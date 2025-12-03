7317e3f56964eb296a16847a307d37f9b40d1b5f
Rajesh Rafaliya <rajeshrafaliya@gmail.com>
2025-12-03 17:59:09 +0530

    Hide Google Analytics

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -96,6 +96,7 @@
     }
    </style>
       <!-- Global site tag (gtag.js) - Google Analytics -->
+<!--       
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153077517-1"></script>
       <script>
          window.dataLayer = window.dataLayer || [];
@@ -107,6 +108,7 @@
          gtag('js', new Date());
          gtag('config', 'UA-153077517-1');
       </script>
+-->
    <!-- <script type="text/javascript">
             $(document).ready(function() { 
                   hideRightsideMenu(); 
--- resources/views/lmslayout.blade.php
+++ resources/views/lmslayout.blade.php
@@ -79,6 +79,7 @@
       }
    </style>
    <!-- Global site tag (gtag.js) - Google Analytics -->
+<!--    
    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153077517-1"></script>
    <script>
       window.dataLayer = window.dataLayer || [];
@@ -90,6 +91,7 @@
       gtag('js', new Date());
       gtag('config', 'UA-153077517-1');
    </script>
+-->   
      <!-- <script type="text/javascript">
             $(document).ready(function() { 
                   hideRightsideMenu(); 
