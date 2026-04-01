c49fe731b906da6b471981c09371c0ee3c22522e
robertparkar99 <133192288+robertparkar99@users.noreply.github.com>
2026-04-01 14:22:07 +0530

    Merge pull request #587 from TrizERP/rp_h5p_contents
    Rp h5p contents add editor

--- routes/lms.php
+++ routes/lms.php
@@ -57,6 +57,7 @@ use App\Http\Controllers\lms\h5p\H5PIndexController;
 use App\Http\Controllers\lms\h5p\H5PScenarioController;
 use App\Http\Controllers\lms\h5p\H5PMCQController;
 use App\Http\Controllers\lms\h5p\H5PInteractiveVideoController;
+use App\Http\Controllers\lms\h5p\H5pFlashcardController;
 
 Route::group(['prefix' => 'lms', 'middleware' => ['session', 'menu', 'logRoute','check_permissions']], function () {
     Route::get('o-net-data-category',[\App\Http\Controllers\lms\ONetOnlineDataController::class,'index'])->name('o-net-data-category.index');
@@ -315,6 +316,7 @@ Route::prefix('h5p')->group(function () {
     Route::resource('scenario_based',H5PScenarioController::class);
     Route::resource('h5p_mcq',H5PMCQController::class);
     Route::resource('h5p_interactive_video',H5PInteractiveVideoController::class);
+    Route::resource('h5p_flashacard',H5pFlashcardController::class);
 });
 Route::post('get-h5p-ai-output', [H5PIndexController::class, 'getH5pAIOutput'])->name('get-h5p-ai-output');
 Route::post('get-h5p-ai-scenario', [H5PScenarioController::class, 'getH5pAIScenario'])->name('get-h5p-ai-scenario');
