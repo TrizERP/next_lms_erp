045f752cd51357dee7c79a00235b171025742628
robertparkar99 <rp2164394@gmail.com>
2026-05-08 16:54:09 +0530

    blocking query issue solve

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Log;
 use App\View\Components\filters;
 
 class AppServiceProvider extends ServiceProvider
@@ -41,17 +42,39 @@ class AppServiceProvider extends ServiceProvider
 
 
         Schema::defaultStringLength(191);
-        
+
+        // DB::listen(function ($query) {
+        // $dangerous = ['DROP', 'TRUNCATE'];
+        // foreach ($dangerous as $cmd) {
+        //     if (stripos($query->sql, $cmd) !== false) {
+        //             Log::critical("DANGEROUS SQL BLOCKED", [
+        //                 'sql' => $query->sql,
+        //                 'user' => auth()->user()?->email ?? 'system',
+        //                 'time' => now()
+        //             ]);
+        //             throw new \Exception("Dangerous command blocked: $cmd");
+        //         }
+        //     }
+        // });
+        // added on 08-05-2026
         DB::listen(function ($query) {
-        $dangerous = ['DROP', 'TRUNCATE'];
-        foreach ($dangerous as $cmd) {
-            if (stripos($query->sql, $cmd) !== false) {
+
+            $dangerousPatterns = [
+                '/\bDROP\s+TABLE\b/i',
+                '/\bTRUNCATE\s+TABLE\b/i',
+            ];
+
+            foreach ($dangerousPatterns as $pattern) {
+
+                if (preg_match($pattern, $query->sql)) {
+
                     Log::critical("DANGEROUS SQL BLOCKED", [
-                        'sql' => $query->sql,
+                        'sql'  => $query->sql,
                         'user' => auth()->user()?->email ?? 'system',
                         'time' => now()
                     ]);
-                    throw new \Exception("Dangerous command blocked: $cmd");
+
+                    throw new \Exception("Dangerous SQL command blocked.");
                 }
             }
         });
