5dfae3817f7b439acb6a75c8583c24127fd1c646
Rajesh <rajesh.rafaliya@triz.co.in>
2026-06-26 12:34:17 +0530

    LIVE_ERP: by Rajsh: 26-06-2026

--- app/Models/lms/lmsOnlineExamAnswerStudent.php
+++ app/Models/lms/lmsOnlineExamAnswerStudent.php
@@ -8,5 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class lmsOnlineExamAnswerStudent extends Model
 {
     use HasFactory;
-    protected $table = "lms_online_exam_answer_student";    
+    protected $table = "lms_online_exam_answer_student";   
+    protected $fillable = [
+        'question_paper_id',
+        'online_exam_id',
+        'student_id',
+        'question_id',
+        'answer_id',
+        'narrative_answer',
+        'ans_status',
+        'attempt_time',
+    ];     
 }
