805759cbc2f5512d42d19a9d129f881f7a7a79ff
robertparkar99 <rp2164394@gmail.com>
2026-07-09 17:01:23 +0530

    lms curriculum new API

--- routes/lms.php
+++ routes/lms.php
@@ -59,6 +59,7 @@ use App\Http\Controllers\lms\h5p\H5PMCQController;
 use App\Http\Controllers\lms\h5p\H5PInteractiveVideoController;
 use App\Http\Controllers\lms\h5p\H5pFlashcardController;
 use App\Http\Controllers\lms\nextAPI\chapterMasterController;
+use App\Http\Controllers\lms\nextAPI\lmsCurriculumController as newCurricuumController;
 
 Route::group(['prefix' => 'lms', 'middleware' => ['session', 'menu', 'logRoute','check_permissions']], function () {
     Route::get('o-net-data-category',[\App\Http\Controllers\lms\ONetOnlineDataController::class,'index'])->name('o-net-data-category.index');
@@ -330,6 +331,7 @@ Route::post('get-h5p-ai-scenario', [H5PScenarioController::class, 'getH5pAIScena
     Route::get('suggested-content', [palController::class, 'suggestedContent'])->name('pal.suggestedContent');
 
 Route::resource('lms/new_chapter_master',chapterMasterController::class);
+Route::resource('lms/new_curriculum',newCurricuumController::class);
 
 
 // use App\Http\Controllers\lms\Neo4jSyncController;
