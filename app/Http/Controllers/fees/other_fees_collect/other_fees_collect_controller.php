f0c4eb7c3c7e2b4063a533933d3b7b20f9fae72d
Rajesh Rafaliya <rajeshrafaliya@gmail.com>
2026-08-07 14:02:11 +0530

    FeesRecieptBookMaster - Status 1

--- app/Http/Controllers/fees/other_fees_collect/other_fees_collect_controller.php
+++ app/Http/Controllers/fees/other_fees_collect/other_fees_collect_controller.php
@@ -272,6 +272,7 @@ class other_fees_collect_controller extends Controller
                 ->selectRaw('*,GROUP_CONCAT(fees_head_id) heads')
                 ->where('syear', $syear)
                 ->where('sub_institute_id', $sub_institute_id)
+                ->where('status', 1)
                 ->groupByRaw('receipt_line_1,receipt_line_2,receipt_line_3,receipt_line_4,receipt_prefix,receipt_logo,last_receipt_number')
                 ->get()->toArray();
 
